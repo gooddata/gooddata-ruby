88d4cf39c7d258aa7cd806bed2037a1c385ea3e5
Thomas Watson Steen <w@tson.dk>
2010-10-08 15:29:55 +0200

    Code clean-up: No need to specify sub-module in two steps

--- lib/good_data/collections/projects.rb
+++ lib/good_data/collections/projects.rb
@@ -1,25 +1,23 @@
-module GoodData
-  module Collections
-    class Projects < Array
-      def create(attributes)
-        GoodData.logger.info "Creating project #{attributes[:name]}"
+module GoodData::Collections
+  class Projects < Array
+    def create(attributes)
+      GoodData.logger.info "Creating project #{attributes[:name]}"
 
-        json = {
-          'meta' => {
-            'title' => attributes[:name],
-            'summary' => attributes[:summary]
-          },
-          'content' => {
-            # 'state' => 'ENABLED',
-            'guidedNavigation' => 1
-          }
+      json = {
+        'meta' => {
+          'title' => attributes[:name],
+          'summary' => attributes[:summary]
+        },
+        'content' => {
+          # 'state' => 'ENABLED',
+          'guidedNavigation' => 1
         }
+      }
 
-        json['mata']['projectTemplate'] = attributes[:template] if attributes.has_key? :template
+      json['mata']['projectTemplate'] = attributes[:template] if attributes.has_key? :template
 
-        self << Project.create(json)
-        last
-      end
+      self << Project.create(json)
+      last
     end
   end
 end
