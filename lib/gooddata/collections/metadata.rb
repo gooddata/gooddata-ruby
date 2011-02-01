5e1cbbe668701b70e53ddcc93b8dc57083c9e92e
Pavel Kolesnikov <pavel@gooddata.com>
2011-02-01 12:59:39 +0100

    CODE: metadata should be indexed by category rather than by title

--- lib/gooddata/collections/metadata.rb
+++ lib/gooddata/collections/metadata.rb
@@ -2,7 +2,7 @@ module GoodData
   class Metadata < Hash
     def initialize(items)
       items['about']['links'].each do |item|
-        self[item['title']] = item['link']
+        self[item['category']] = item['link']
       end
     end
   end
