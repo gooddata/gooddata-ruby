670ae39321ea802653436b8f6fde903448292c4d
Tomas Svarovsky <svarovsky@gooddata.com>
2011-07-20 15:10:04 -0700

    CODE: Schema parsing changes, fixing facts, added more ldm types.
    Added support for LABEL, REFERENCE and DATE

--- lib/gooddata/model.rb
+++ lib/gooddata/model.rb
@@ -6,7 +6,9 @@ require 'fastercsv'
 # elements, including the server-side data model.
 #
 module GoodData
+
   module Model
+
     # GoodData REST API categories
     LDM_CTG = 'ldm'
     LDM_MANAGE_CTG = 'ldm-manage'
@@ -20,11 +22,18 @@ module GoodData
     LABEL_COLUMN_PREFIX = 'nm_'
     ATTRIBUTE_FOLDER_PREFIX = 'dim'
     ATTRIBUTE_PREFIX = 'attr'
+    LABEL_PREFIX = 'label'
     FACT_PREFIX = 'fact'
     DATE_FACT_PREFIX = 'dt'
+    DATE_ATTRIBUTE = "date"
+    DATE_ATTRIBUTE_DEFAULT_DISPLAY_FORM = 'mdyy'
     TIME_FACT_PREFIX = 'tm.dt'
     TIME_ATTRIBUTE_PREFIX = 'attr.time'
     FACT_FOLDER_PREFIX = 'ffld'
+
+    SKIP_FIELD = false
+
+    BEGINNING_OF_TIMES = Date.parse('1/1/1900')
 
     class << self
       def add_dataset(title, columns, project = nil)
@@ -74,7 +83,7 @@ module GoodData
     # model abstractions.
     #
     class Schema < MdObject
-      attr_reader :fields
+      attr_reader :fields, :attributes, :facts, :folders, :references, :labels
 
       def self.load(file)
         Schema.new JSON.load(open(file))
@@ -82,20 +91,57 @@ module GoodData
 
       def initialize(config, title = nil)
         @fields = []
+        @attributes = {}
+        @facts = {}
+        @folders = {
+          :facts      => {},
+          :attributes => {}
+        }
+        @references = {}
+        @labels = []
+
         config['title'] = title unless config['title']
         raise 'Schema name not specified' unless config['title']
         self.title = config['title']
         self.config = config
       end
 
+      def transform_header(headers)
+        result = fields.reduce([]) do |memo, f|
+          val = f.to_csv_header(headers)
+          memo << val unless val === SKIP_FIELD
+          memo
+        end
+        result.flatten
+      end
+
+      def transform_row(headers, row)
+        result = fields.reduce([]) do |memo, f|
+          val = f.to_csv_data(headers, row)
+          memo << val unless val === SKIP_FIELD
+          memo
+        end
+        result.flatten
+      end
+
       def config=(config)
-        labels = []
         config['columns'].each do |c|
-          add_attribute c if c['type'] == 'ATTRIBUTE'
-          add_fact c if c['type'] == 'FACT'
-          add_date c if c['type'] == 'DATE'
-          set_connection_point c if c['type'] == 'CONNECTION_POINT'
-          labels.push c if c['type'] == 'LABEL'
+          case c['type']
+          when 'ATTRIBUTE'
+            add_attribute c
+          when 'FACT'
+            add_fact c
+          when 'DATE'
+            add_date c
+          when 'CONNECTION_POINT'
+            set_connection_point c
+          when 'LABEL'
+            add_label c
+          when 'REFERENCE'
+            add_reference c
+          else
+            fail "Unexpected type #{c['type']} in #{c.inspect}"
+          end
         end
         @connection_point = RecordsOf.new(nil, self) unless @connection_point
       end
@@ -106,10 +152,6 @@ module GoodData
       end
 
       def type_prefix ; 'dataset' ; end
-
-      def attributes; @attributes ||= {} ; end
-      def facts; @facts ||= {} ; end
-      def folders; @folders ||= {}; end
 
       ##
       # Underlying fact table name
@@ -142,8 +184,19 @@ module GoodData
             maql += "ALTER DATASET {#{self.identifier}} ADD {#{obj.identifier}};\n\n"
           end
         end
+        
+        labels.each do |label|
+          maql += "# Creating Labels\n"
+          maql += label.to_maql_create
+        end
+        
+        references.values.each do |ref|
+          maql += "# Creating references\n"
+          maql += ref.to_maql_create
+        end
+        
         folders_maql = "# Create folders\n"
-        folders.keys.each { |folder| folders_maql += folder.to_maql_create }
+        (folders[:attributes].values + folders[:facts].values).each { |folder| folders_maql += folder.to_maql_create }
         folders_maql + "\n" + maql + "SYNCHRONIZE {#{identifier}};\n"
       end
 
@@ -151,37 +204,49 @@ module GoodData
       #
       def upload(path, project = nil)
         path = path.path if path.respond_to? :path
+        header = nil
         project = GoodData.project unless project
-
+      
         # create a temporary zip file
         dir = Dir.mktmpdir
         Zip::ZipFile.open("#{dir}/upload.zip", Zip::ZipFile::CREATE) do |zip|
           # TODO make sure schema columns match CSV column names
           zip.get_output_stream('upload_info.json') { |f| f.puts JSON.pretty_generate(to_manifest) }
           zip.get_output_stream('data.csv') do |f|
-            FasterCSV.foreach(path) { |row| f.puts row.to_csv }
+            FasterCSV.foreach(path, :headers => true, :return_headers => true) do |row|
+              output = if row.header_row?
+                transform_header(row)
+              else
+                transform_row(header, row)
+              end
+              f.puts output.to_csv
+            end
           end
         end
-
+      
         # upload it
         GoodData.connection.upload "#{dir}/upload.zip", File.basename(dir)
         FileUtils.rm_rf dir
-
+      
         # kick the load
         pull = { 'pullIntegration' => File.basename(dir) }
         link = project.md.links('etl')['pull']
-        GoodData.post link, pull
+        task = GoodData.post link, pull
+        while GoodData.get(task["pullTask"]["uri"])["taskStatus"] === "RUNNING" do
+          sleep 30
+        end
+        puts "Done loading"
       end
 
       # Generates the SLI manifest describing the data loading
-      #
+      # 
       def to_manifest
         {
           'dataSetSLIManifest' => {
-            'parts'   => fields.map { |f| f.to_manifest_part },
+            'parts'   => fields.reduce([]) { |memo, f| val = f.to_manifest_part; memo << val unless val.nil?; memo },
             'dataSet' => self.identifier,
             'file'    => 'data.csv', # should be configurable
-            'csvParams' => {
+              'csvParams' => {
               'quoteChar'     => '"',
               'escapeChar'    => '"',
               'separatorChar' => ',',
@@ -195,24 +260,51 @@ module GoodData
 
       def add_attribute(column)
         attribute = Attribute.new column, self
-        @fields << attribute
-        add_to_hash(self.attributes, attribute)
-        folders[AttributeFolder.new(attribute.folder)] = 1 if attribute.folder
+        fields << attribute
+        add_to_hash(attributes, attribute)
+        add_attribute_folder(attribute.folder)
+        # folders[AttributeFolder.new(attribute.folder)] = 1 if attribute.folder 
+      end
+
+      def add_attribute_folder(name)
+        return if name.nil?
+        return if folders[:attributes].has_key?(name)
+        folders[:attributes][name] = AttributeFolder.new(name)
       end
 
       def add_fact(column)
         fact = Fact.new column, self
-        @fields << fact
-        add_to_hash(self.facts, fact)
-        folders[FactFolder.new(fact.folder)] = 1 if fact.folder
+        fields << fact
+        add_to_hash(facts, fact)
+        add_fact_folder(fact.folder)
+        # folders[FactFolder.new(fact.folder)] = 1 if fact.folder
+      end
+
+      def add_fact_folder(name)
+        return if name.nil?
+        return if folders[:facts].has_key?(name)
+        folders[:facts][name] = FactFolder.new(name)
+      end
+
+      def add_label(column)
+        label = Label.new(column, nil, self)
+        labels << label
+        fields << label
+      end
+
+      def add_reference(column)
+        reference = Reference.new(column, self)
+        fields << reference
+        add_to_hash(references, reference)
       end
 
       def add_date(column)
         date = DateColumn.new column, self
+        @fields << date
         date.parts.values.each { |p| @fields << p }
         date.facts.each { |f| add_to_hash(self.facts, f) }
         date.attributes.each { |a| add_to_hash(self.attributes, a) }
-        @fields << date
+        date.references.each {|r| add_to_hash(self.references, r)}
       end
 
       def set_connection_point(column)
@@ -255,6 +347,15 @@ module GoodData
         visual += ", FOLDER {#{folder_prefix}.#{Model::to_id(folder)}}" if folder
         visual
       end
+
+      def to_csv_header(row)
+        name
+      end
+
+      def to_csv_data(headers, row)
+        row[name]
+      end
+      
 
       # Overriden to prevent long strings caused by the @schema attribute
       #
@@ -310,21 +411,23 @@ module GoodData
     class Label < Column
       def type_prefix ; 'label' ; end
 
+      # def initialize(hash, schema)
       def initialize(hash, attribute, schema)
         super hash, schema
-        @attribute = attribute
+        @attribute = attribute || schema.fields.find {|field| field.name === hash["reference"]}
       end
 
       def to_maql_create
+        "# LABEL FROM LABEL"
         "ALTER ATTRIBUTE {#{@attribute.identifier}} ADD LABELS {#{identifier}}" \
               + " VISUAL (TITLE #{title.inspect}) AS {#{column}};\n"
       end
 
       def to_manifest_part
         {
-          'populates'  => [ identifier ],
-          'mode'       => 'FULL',
-          'columnName' => name
+          'populates'     => [ identifier ],
+          'mode'          => 'FULL',
+          'columnName'    => name
         }
       end
 
@@ -394,7 +497,7 @@ module GoodData
         {
           'populates'  => [ identifier ],
           'mode'       => 'FULL',
-          'columnName' => column
+          'columnName' => name
         }
       end
     end
@@ -402,11 +505,14 @@ module GoodData
     ##
     # Reference to another data set
     #
-    class Reference
+    class Reference < Column
       def initialize(column, schema)
+        super column, schema
+        # pp column
+        
         @name       = column['name']
         @reference  = column['reference']
-        @schema_ref = column['schema_ref']
+        @schema_ref = column['schema_reference']
         @schema     = schema
       end
 
@@ -416,28 +522,29 @@ module GoodData
       # from the reference key.
       #
       def identifier
-        @identifier ||= "#{ATTRIBUTE_PREFIX}.#{Model::to_id @schema_ref.title}.#{Model::to_id @reference}"
+        @identifier ||= "#{ATTRIBUTE_PREFIX}.#{Model::to_id @schema_ref}.#{Model::to_id @reference}"
       end
 
       def key ; "#{Model::to_id @name}_id" ; end
 
       def label_column
-        @column ||= "#{@schema.table}.#{LABEL_COLUMN_PREFIX Model::to_id(reference)}"
+        "#{LABEL_PREFIX}.#{Model::to_id @schema_ref}.#{Model::to_id @reference}"
       end
 
       def to_maql_create
-        "ALTER ATTRIBUTE {#{self.identifier} ADD KEYS {#{@schema.table}.#{key}}"
+        "ALTER ATTRIBUTE {#{self.identifier}} ADD KEYS {#{@schema.table}.#{key}};\n"
       end
 
       def to_maql_drop
-        "ALTER ATTRIBUTE {#{self.identifier} DROP KEYS {#{@schema.table}.#{key}}"
+        "ALTER ATTRIBUTE {#{self.identifier} DROP KEYS {#{@schema.table}.#{key}};\n"
       end
 
       def to_manifest_part
         {
-          'populates'  => [ identifier ],
-          'mode'       => 'FULL',
-          'columnName' => label_column
+          'populates'     => [ label_column ],
+          'mode'          => 'FULL',
+          'columnName'    => name,
+          'referenceKey'  => 1
         }
       end
     end
@@ -446,8 +553,35 @@ module GoodData
     # Fact representation of a date.
     #
     class DateFact < Fact
+      
+      attr_accessor :format, :output_format
+
+      def initialize(column, schema)
+        super column, schema
+        @output_format = column["format"] || '("dd/MM/yyyy")'
+        @format = @output_format.gsub('yyyy', '%Y').gsub('MM', '%m').gsub('dd', '%d')
+      end
+
       def column_prefix ; DATE_COLUMN_PREFIX ; end
       def type_prefix ; DATE_FACT_PREFIX ; end
+
+      def to_csv_header(row)
+        "#{name}_fact"
+      end
+
+      def to_csv_data(headers, row)
+        val = row[name]
+        val.nil?() ? nil : (Date.strptime(val, format) - BEGINNING_OF_TIMES).to_i
+      end
+
+      def to_manifest_part
+        {
+          'populates'  => [ identifier ],
+          'mode'       => 'FULL',
+          'columnName' => "#{name}_fact"
+        }
+      end
+
     end
 
     ##
@@ -455,6 +589,38 @@ module GoodData
     #
     class DateReference < Reference
 
+      attr_accessor :format, :output_format, :urn
+
+      def initialize(column, schema)
+        super column, schema
+        @output_format = column["format"] || '("dd/MM/yyyy")'
+        @format = @output_format.gsub('yyyy', '%Y').gsub('MM', '%m').gsub('dd', '%d')
+        @urn = column["urn"] || "URN:GOODDATA:DATE"
+      end
+
+      def identifier
+        @identifier ||= "#{Model::to_id @schema_ref}.#{DATE_ATTRIBUTE}"
+      end
+
+      def to_manifest_part
+        {
+          'populates'     => [ "#{identifier}.#{DATE_ATTRIBUTE_DEFAULT_DISPLAY_FORM}" ],
+          'mode'          => 'FULL',
+          'constraints'   => {"date" => output_format},
+          'columnName'    => name,
+          'referenceKey'  => 1
+        }
+      end
+      
+      def to_maql_create
+        # urn:chefs_warehouse_fiscal:date
+        super_maql = super
+        maql = ""
+        # maql = "# Include date dimensions\n"
+        # maql += "INCLUDE TEMPLATE \"#{urn}\" MODIFY (IDENTIFIER \"#{name}\", TITLE \"#{title || name}\");\n"
+        maql += super_maql
+      end
+      
     end
 
     ##
@@ -462,6 +628,15 @@ module GoodData
     #
     class DateAttribute < Attribute
       def key ; "#{DATE_COLUMN_PREFIX}#{super}" ; end
+
+      def to_manifest_part
+        {
+          'populates'     => ['label.stuff.mmddyy'],
+          "format"        => "unknown",
+          "mode"          => "full",
+          "referenceKey"  => 1
+        }
+      end
     end
 
     ##
@@ -493,22 +668,24 @@ module GoodData
     # parts: date fact, a date reference or attribute and an optional time component
     # that contains a time fact and a time reference or attribute.
     #
-    class DateColumn
-      attr_reader :parts, :facts, :attributes
+    class DateColumn < Column
+      attr_reader :parts, :facts, :attributes, :references
 
       def initialize(column, schema)
-        @parts = {} ; @facts = [] ; @attributes = []
+        super column, schema
+        @parts = {} ; @facts = [] ; @attributes = []; @references = []
 
         @facts << @parts[:date_fact] = DateFact.new(column, schema)
-        if column['schemaReference'] then
+        if column['schema_reference'] then
           @parts[:date_ref] = DateReference.new column, schema
+          @references << @parts[:date_ref]
         else
           @attributes << @parts[:date_attr] = DateAttribute.new(column, schema)
         end
         if column['datetime'] then
           puts "*** datetime"
           @facts << @parts[:time_fact] = TimeFact.new(column, schema)
-          if column['schemaReference'] then
+          if column['schema_reference'] then
             @parts[:time_ref] = TimeReference.new column, schema
           else
             @attributes << @parts[:time_attr] = TimeAttribute.new(column, schema)
@@ -523,6 +700,19 @@ module GoodData
       def to_maql_drop
         @parts.values.map { |v| v.to_maql_drop }.join "\n"
       end
+
+      def to_csv_header(row)
+        SKIP_FIELD
+      end
+
+      def to_csv_data(headers, row)
+        SKIP_FIELD
+      end
+
+      def to_manifest_part
+        nil
+      end
+
     end
 
     ##
@@ -555,5 +745,16 @@ module GoodData
       def type; "FACT"; end
       def type_prefix; "ffld"; end
     end
+    
+    class DateDimension < MdObject
+      
+      def to_maql_create
+        # urn:chefs_warehouse_fiscal:date
+        maql = ""
+        maql += "INCLUDE TEMPLATE \"#{urn}\" MODIFY (IDENTIFIER \"#{name}\", TITLE \"#{title || name}\");"
+        maql
+      end
+    end
+    
   end
 end
