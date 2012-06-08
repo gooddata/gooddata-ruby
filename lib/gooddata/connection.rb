9d9240ffb0cf2cb230c37350c527440a34372ad0
Tomas Svarovsky <svarovsky.tomas@gmail.com>
2012-06-08 13:41:10 +0200

    Added retryable

--- lib/gooddata/connection.rb
+++ lib/gooddata/connection.rb
@@ -34,6 +34,30 @@ module GoodData
     TOKEN_PATH = '/gdc/account/token'
     STAGE_PATH = '/uploads/'
 
+    # Options:
+    # * :tries - Number of retries to perform. Defaults to 1.
+    # * :on - The Exception on which a retry will be performed. Defaults to Exception, which retries on any Exception.
+    #
+    # Example
+    # =======
+    #   retryable(:tries => 1, :on => OpenURI::HTTPError) do
+    #     # your code here
+    #   end
+    #
+    def retryable(options = {}, &block)
+      opts = { :tries => 1, :on => Exception }.merge(options)
+
+      retry_exception, retries = opts[:on], opts[:tries]
+
+      begin
+        return yield
+      rescue retry_exception
+        retry if (retries -= 1) > 0
+      end
+
+      yield
+    end
+
     # Set the GoodData account credentials.
     #
     # This have to be performed before any calls to the API.
