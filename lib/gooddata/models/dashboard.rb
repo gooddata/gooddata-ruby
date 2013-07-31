c6bb165140962f0f97dff600edc9d5db2f08b2e3
Tomas Svarovsky <svarovsky.tomas@gmail.com>
2013-07-31 00:06:19 -0700

    Removed hardcoded project id

--- lib/gooddata/models/dashboard.rb
+++ lib/gooddata/models/dashboard.rb
@@ -15,7 +15,7 @@ module GoodData
       supported_formats = [:pdf]
       fail "Wrong format provied \"#{format}\". Only supports formats #{supported_formats.join(', ')}" unless supported_formats.include?(format)
       tab = options[:tab] || ""
-      x = GoodData.post("/gdc/projects/lbhjnmbwa07f9g46hnjp3l5c9hphnev9/clientexport", {"clientExport" => {"url" => "https://secure.gooddata.com/dashboard.html#project=#{GoodData.project.uri}&dashboard=#{uri}&tab=#{tab}&export=1", "name" => title}}, :process => false)
+      x = GoodData.post("/gdc/projects/#{GoodData.project.uri}/clientexport", {"clientExport" => {"url" => "https://secure.gooddata.com/dashboard.html#project=#{GoodData.project.uri}&dashboard=#{uri}&tab=#{tab}&export=1", "name" => title}}, :process => false)
       while (x.code == 202) do
         sleep(1)
         uri = JSON.parse(x.body)["asyncTask"]["link"]["poll"]
