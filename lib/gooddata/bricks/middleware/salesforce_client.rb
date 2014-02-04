587a2a87eecd3735d95222cad13d7337ec78fa15
Tomas Svarovsky <svarovsky.tomas@gmail.com>
2014-02-03 19:17:03 -0800

    Adding SF middleware

---
+++ lib/gooddata/bricks/middleware/salesforce_client.rb
@@ -0,0 +1,27 @@
+require 'gooddata'
+
+module GoodData::Bricks
+  class RestForceMiddleware < GoodData::Bricks::Middleware
+
+    def call(params)
+      
+
+      username      = params["salesforce_username"]
+      password      = params["salesforce_password"]
+      token         = params["salesforce_token"]
+      client_id     = params["salesforce_client_id"]
+      client_secret = params["salesforce_client_secret"]
+      
+      client = Restforce.new(
+        :username       => username,
+        :password       => password,
+        :security_token => token,
+
+        :client_id      => client_id,
+        :client_secret  => client_secret)
+
+      @app.call(params.merge(:salesforce_client => client))
+    end
+
+  end
+end
