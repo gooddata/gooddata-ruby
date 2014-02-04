20e30e66d1df4dc531e0db960a8bb4d5d263346f
Tomas Svarovsky <svarovsky.tomas@gmail.com>
2014-02-03 18:38:52 -0800

    Added project metadata object

---
+++ lib/gooddata/models/project_metadata.rb
@@ -0,0 +1,42 @@
+module GoodData
+  class ProjectMetadata
+
+    class << self
+      def [](key)
+        if key == :all
+          GoodData.get("/gdc/projects/#{GoodData.project.pid}/dataload/metadata")
+        else 
+          GoodData.get("/gdc/projects/#{GoodData.project.pid}/dataload/metadata/#{key}")
+        end
+      end
+
+      def has_key?(key)
+        begin
+          ProjectMetadata[key]
+          true
+        rescue RestClient::ResourceNotFound => e
+          false
+        end
+      end
+
+      def []=(key, val)
+        data = {
+          :metadataItem => {
+           :key => key,
+           :value => val
+         }
+        }
+        uri = "/gdc/projects/#{GoodData.project.pid}/dataload/metadata/"
+        update_uri = uri + key
+
+        if has_key?(key)
+          GoodData.put(update_uri, data)
+        else
+          GoodData.post(uri, data)
+        end
+      end
+
+    end
+
+  end
+end
