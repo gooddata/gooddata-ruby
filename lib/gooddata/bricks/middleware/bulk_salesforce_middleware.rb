16ca071c9bc3d8b74d7c48ab83d9032d32c88e1d
Tomas Svarovsky <svarovsky.tomas@gmail.com>
2014-02-17 21:12:01 -0800

    Added bulk sf middleware

---
+++ lib/gooddata/bricks/middleware/bulk_salesforce_middleware.rb
@@ -0,0 +1,39 @@
+require 'gooddata'
+require 'salesforce_bulk'
+
+module GoodData::Bricks
+  class BulkSalesforceMiddleware < GoodData::Bricks::Middleware
+
+    def call(params)
+
+      username      = params[:salesforce_username]
+      password      = params[:salesforce_password]
+      token         = params[:salesforce_token]
+      client_id     = params[:salesforce_client_id]
+      client_secret = params[:salesforce_client_secret]
+      host          = params[:salesforce_host]
+
+      credentials = {}
+
+      credentials = if (username && password && token)
+        {
+          :username       => username,
+          :password       => password,
+          :security_token => token
+        }
+      end
+
+      client = if credentials
+        credentials.merge!({
+          :client_id      => client_id,
+          :client_secret  => client_secret,
+        })
+        credentials[:host] = host unless host.nil?
+        binding.pry
+        SalesforceBulk::Api.new(credentials[:username], credentials[:password] + credentials[:security_token])
+      end
+      @app.call(params.merge(:salesforce_bulk_client => client))
+    end
+
+  end
+end
