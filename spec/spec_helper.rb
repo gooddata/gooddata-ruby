d0f02a8c59d6580afcf63c3b630900093a1cd52f
korczis <korczis@gmail.com>
2014-03-12 15:03:56 +0100

    Moved GD_PROJECT_TOKEN to spec_helper to prevent multiple initialization and to keep it DRY

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,10 @@
+require 'rspec'
+
 # Automagically include all helpers/*_helper.rb
 require 'helpers/blueprint_helper'
 
 RSpec.configure do |config|
   include BlueprintHelper
+
+  GD_PROJECT_TOKEN = ENV["GD_PROJECT_TOKEN"]
 end
