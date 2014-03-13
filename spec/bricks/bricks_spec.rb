77efb30b486d1f78a980e46e736e45e34de63240
korczis <korczis@gmail.com>
2014-03-13 11:38:02 +0100

    Remove old test code/spec

--- spec/bricks/bricks_spec.rb
+++ spec/bricks/bricks_spec.rb
@@ -1,18 +1,8 @@
+require 'gooddata/bricks/brick'
 require 'gooddata/bricks/bricks'
 
 describe GoodData::Bricks do
   it "Has GoodData::Bricks::Brick class" do
     GoodData::Bricks::Brick.should_not == nil
-  end
-end
-
-class String
-  def pigize
-  end
-end
-
-describe String, "#pigize" do
-  it "Pigizes string" do
-    1.should == 1
   end
 end
