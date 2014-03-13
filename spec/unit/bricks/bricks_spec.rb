869d1d1cd8c26bfe80476f68e195b26787926c73
korczis <korczis@gmail.com>
2014-03-13 22:38:33 +0100

    It should be possible to execute a brick

--- spec/unit/bricks/bricks_spec.rb
+++ spec/unit/bricks/bricks_spec.rb
@@ -1,8 +1,25 @@
 require 'gooddata/bricks/brick'
 require 'gooddata/bricks/bricks'
+require 'gooddata/bricks/middleware/middleware'
 
 describe GoodData::Bricks do
   it "Has GoodData::Bricks::Brick class" do
     GoodData::Bricks::Brick.should_not == nil
+  end
+
+  it "should be possible to execute a brick" do
+
+    class DummyBrick < GoodData::Bricks::Brick
+
+      def call(params)
+        puts "hello"
+      end
+    end
+
+    include GoodData::Bricks
+
+    p = GoodData::Bricks::Pipeline.prepare([GoodData::Bricks::BenchMiddleware, DummyBrick])
+
+    p.call({})
   end
 end
