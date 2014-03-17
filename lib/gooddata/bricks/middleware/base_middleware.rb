ac688e4640b9aace78099cf99e2359a42de2f1b8
Tomas Svarovsky <svarovsky.tomas@gmail.com>
2014-03-17 10:11:43 +0100

    Ability to initialize a middleware without knowing the app. Accessor to beable to set it later

--- lib/gooddata/bricks/middleware/base_middleware.rb
+++ lib/gooddata/bricks/middleware/base_middleware.rb
@@ -1,11 +1,13 @@
 module GoodData::Bricks
 
   class Middleware
+    attr_accessor :app
+
     include GoodData::Bricks::Utils
 
-    def initialize(app)
+    def initialize(app=nil)
       @app = app
     end
-    
+
   end
 end
