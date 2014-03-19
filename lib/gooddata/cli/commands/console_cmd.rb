ff61e8bc055f0ee1fb9e41d1070a6669d306f2c2
Tomas Svarovsky <svarovsky.tomas@gmail.com>
2014-03-19 14:56:07 +0100

    Added console command which kicks into a pry console with preloaded gooddata gem

---
+++ lib/gooddata/cli/commands/console_cmd.rb
@@ -0,0 +1,17 @@
+require 'pp'
+
+require File.join(File.dirname(__FILE__), "../shared")
+
+GoodData::CLI.module_eval do
+  desc 'Interactive session with gooddata sdk loaded'
+  command :console do |c|
+
+    c.action do |global_options, options, args|
+      puts "Use 'exit' to quit the live session. Use 'q' to jump out of displaying a large output."
+      binding.pry(:quiet => true,
+                  :prompt => [proc { |target_self, nest_level, pry|
+                    "sdk_live_sesion: "
+                  }])
+    end
+  end
+end
