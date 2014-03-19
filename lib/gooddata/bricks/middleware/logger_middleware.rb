9d8455cb327a1fa2e109fa6be5f0d6bfaf45bd62
Tomas Svarovsky <svarovsky.tomas@gmail.com>
2014-03-19 10:02:28 +0100

    Fixing the logger instantiation in GDC middleware

--- lib/gooddata/bricks/middleware/logger_middleware.rb
+++ lib/gooddata/bricks/middleware/logger_middleware.rb
@@ -6,9 +6,8 @@ module GoodData::Bricks
   class LoggerMiddleware < GoodData::Bricks::Middleware
 
     def call(params)
-      logger = params[:gdc_logger] = params[:GDC_LOGGER_FILE].nil? ? Logger.new(STDOUT) : Logger.new(params[:GDC_LOGGER_FILE])
+      logger = params["GDC_LOGGER"] = params[:GDC_LOGGER_FILE].nil? ? Logger.new(STDOUT) : Logger.new(params[:GDC_LOGGER_FILE])
       logger.info("Pipeline starts")
-
       returning(@app.call(params)) do |result|
         logger.info("Pipeline ending")
       end
