90c7452f322853712cde4644f738112f4e01e915
Tomas Korcak <tomas.korcak@gooddata.com>
2014-03-20 11:28:54 +0100

    Test api and project subcommands

--- spec/unit/cli/commands/cmd_api_spec.rb
+++ spec/unit/cli/commands/cmd_api_spec.rb
@@ -2,9 +2,19 @@ require 'gooddata/cli/cli'
 
 describe GoodData::CLI do
   it "Has working 'api' command" do
-    args = [
-        'api'
-    ]
+    args = %w(api)
+
+    GoodData::CLI.main(args)
+  end
+
+  it "Has working 'api info' command" do
+    args = %w(api info)
+
+    GoodData::CLI.main(args)
+  end
+
+  it "Has working 'api get /gdc' command" do
+    args = %w(api get /gdc)
 
     GoodData::CLI.main(args)
   end
--- spec/unit/cli/commands/cmd_project_spec.rb
+++ spec/unit/cli/commands/cmd_project_spec.rb
@@ -2,10 +2,59 @@ require 'gooddata/cli/cli'
 
 describe GoodData::CLI do
   it "Has working 'project' command" do
-    args = [
-        'project'
-    ]
+    args = %w(project)
 
     GoodData::CLI.main(args)
   end
+
+  it "Has working 'project build' command" do
+    args = %w(project build)
+
+    GoodData::CLI.main(args)
+  end
+
+  it "Has working 'project clone' command" do
+    args = %w(project clone)
+
+    GoodData::CLI.main(args)
+  end
+
+  it "Has working 'project create' command" do
+    args = %w(project create)
+
+    # TODO: Pass all required args to prevent interaction
+    # TODO: Investigate, fix and enable execution
+    # GoodData::CLI.main(args)
+  end
+
+  it "Has working 'project delete' command" do
+    args = %w(project delete)
+
+    GoodData::CLI.main(args)
+  end
+
+  it "Has working 'project jack_in' command" do
+    args = %w(project jack_in)
+
+    GoodData::CLI.main(args)
+  end
+
+  it "Has working 'project list' command" do
+    args = %w(project list)
+
+    GoodData::CLI.main(args)
+  end
+
+  it "Has working 'project show' command" do
+    args = %w(project show)
+
+    GoodData::CLI.main(args)
+  end
+
+  it "Has working 'project update' command" do
+    args = %w(project update)
+
+    GoodData::CLI.main(args)
+  end
+
 end
