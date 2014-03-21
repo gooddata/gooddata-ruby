8a087099dfe46b229fe9bac50f2a077169e95292
korczis <korczis@gmail.com>
2014-03-21 13:07:16 +0100

    sanitize_string method

--- lib/gooddata/helpers.rb
+++ lib/gooddata/helpers.rb
@@ -1,46 +1,57 @@
 # encoding: UTF-8
 
-module GoodData::Helpers
-  def self.home_directory
-    running_on_windows? ? ENV['USERPROFILE'] : ENV['HOME']
-  end
+require 'active_support/inflections'
 
-  def self.running_on_windows?
-    RUBY_PLATFORM =~ /mswin32|mingw32/
-  end
-
-  def self.running_on_a_mac?
-    RUBY_PLATFORM =~ /-darwin\d/
-  end
-
-  def self.error(msg)
-    STDERR.puts(msg)
-    exit 1
-  end
-
-  def self.find_goodfile(pwd, options={})
-    root = Pathname(options[:root] || '/')
-    pwd = Pathname(pwd).expand_path
-    begin
-      gf = pwd + 'Goodfile'
-      if gf.exist?
-        return gf
+module GoodData
+  module Helpers
+    class << self
+      def home_directory
+        running_on_windows? ? ENV['USERPROFILE'] : ENV['HOME']
       end
-      pwd = pwd.parent
-    end until root == pwd
-    nil
-  end
 
-  def self.hash_dfs(thing, &block)
-    if !thing.is_a?(Hash) && !thing.is_a?(Array)
-    elsif thing.is_a?(Array)
-      thing.each do |child|
-        hash_dfs(child, &block)
+      def running_on_windows?
+        RUBY_PLATFORM =~ /mswin32|mingw32/
       end
-    else
-      thing.each do |key, val|
-        yield(thing, key)
-        hash_dfs(val, &block)
+
+      def running_on_a_mac?
+        RUBY_PLATFORM =~ /-darwin\d/
+      end
+
+      def error(msg)
+        STDERR.puts(msg)
+        exit 1
+      end
+
+      def find_goodfile(pwd, options={})
+        root = Pathname(options[:root] || '/')
+        pwd = Pathname(pwd).expand_path
+        begin
+          gf = pwd + 'Goodfile'
+          if gf.exist?
+            return gf
+          end
+          pwd = pwd.parent
+        end until root == pwd
+        nil
+      end
+
+      def hash_dfs(thing, &block)
+        if !thing.is_a?(Hash) && !thing.is_a?(Array)
+        elsif thing.is_a?(Array)
+          thing.each do |child|
+            hash_dfs(child, &block)
+          end
+        else
+          thing.each do |key, val|
+            yield(thing, key)
+            hash_dfs(val, &block)
+          end
+        end
+      end
+
+      def sanitize_string(str)
+        str = ActiveSupport::Inflector.transliterate(str)
+        str.gsub(/[^a-z]/, '')
       end
     end
   end
