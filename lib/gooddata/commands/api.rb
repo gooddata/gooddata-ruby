eba858e8327eb8054ffbcd4da8691f5cd1de6085
korczis <korczis@gmail.com>
2014-03-24 14:13:23 +0100

    Improved test of low-level API command

--- lib/gooddata/commands/api.rb
+++ lib/gooddata/commands/api.rb
@@ -2,42 +2,50 @@
 
 require_relative '../exceptions/command_failed'
 
-module GoodData::Command
-  class Api
-    class << self
-      def info
-        json = {
-          'releaseName' => 'N/A',
-          'releaseDate' => 'N/A',
-          'releaseNotesUri' => 'N/A'
-        }
+module GoodData
+  module Command
+    # Low level access to GoodData API
+    class Api
+      class << self
+        def info
+          json = {
+            'releaseName' => 'N/A',
+            'releaseDate' => 'N/A',
+            'releaseNotesUri' => 'N/A'
+          }
 
-        puts 'GoodData API'
-        puts "  Version: #{json['releaseName']}"
-        puts "  Released: #{json['releaseDate']}"
-        puts "  For more info see #{json['releaseNotesUri']}"
-      end
-
-      alias :index :info
-
-      def test
-        if GoodData.test_login
-          puts "Succesfully logged in as #{GoodData.profile.user}"
-        else
-          puts 'Unable to log in to GoodData server!'
+          puts 'GoodData API'
+          puts "  Version: #{json['releaseName']}"
+          puts "  Released: #{json['releaseDate']}"
+          puts "  For more info see #{json['releaseNotesUri']}"
         end
-      end
 
-      def get(path)
-        raise(GoodData::CommandFailed, 'Specify the path you want to GET.') if path.nil?
-        result = GoodData.get path
-        result rescue puts result
-      end
+        alias_method :index, :info
 
-      def delete(path)
-        raise(GoodData::CommandFailed, 'Specify the path you want to DELETE.') if path.nil?
-        result = GoodData.delete path
-        result rescue puts result
+        # Test of login
+        def test
+          if GoodData.test_login
+            puts "Succesfully logged in as #{GoodData.profile.user}"
+          else
+            puts 'Unable to log in to GoodData server!'
+          end
+        end
+
+        # Get resource
+        # @param path Resource path
+        def get(path)
+          raise(GoodData::CommandFailed, 'Specify the path you want to GET.') if path.nil?
+          result = GoodData.get path
+          result rescue puts result
+        end
+
+        # Delete resource
+        # @param path Resource path
+        def delete(path)
+          raise(GoodData::CommandFailed, 'Specify the path you want to DELETE.') if path.nil?
+          result = GoodData.delete path
+          result rescue puts result
+        end
       end
     end
   end
--- spec/unit/commands/command_api_spec.rb
+++ spec/unit/commands/command_api_spec.rb
@@ -5,8 +5,27 @@ describe GoodData::Command::Api do
     @connection = ConnectionHelper::create_default_connection
   end
 
-  it "Is Possible to create GoodData::Command::Profile instance" do
+  it "Is Possible to create GoodData::Command::Api instance" do
     cmd = GoodData::Command::Api.new()
     cmd.should be_a(GoodData::Command::Api)
   end
+
+  describe "#get" do
+    it "Call without arguments" do
+      GoodData::Command::Api.test()
+    end
+  end
+
+  describe "#delete" do
+    it "Call without arguments" do
+      GoodData::Command::Api.test()
+    end
+  end
+
+  describe "#test" do
+    it "Call without arguments" do
+      GoodData::Command::Api.test()
+    end
+  end
+
 end
