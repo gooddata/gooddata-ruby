a1e0ca4b132a7401b9d142bd2a27e1209234dfd9
korczis <korczis@gmail.com>
2014-03-24 12:50:46 +0100

    Do not use .simplecov as it causes mysterious troubles with rspec via coveralls

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,3 +16,19 @@ RSpec.configure do |config|
   include CliHelper
   include ConnectionHelper
 end
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+  SimpleCov::Formatter::HTMLFormatter,
+  Coveralls::SimpleCov::Formatter
+]
+
+SimpleCov.start do
+  # add_filter 'spec/'
+  # add_filter 'test/'
+
+  add_group 'Bricks', 'lib/gooddata/bricks'
+  add_group 'CLI', 'lib/gooddata/cli'
+  add_group 'Commands', 'lib/gooddata/commands'
+  add_group 'Goodzilla', 'lib/gooddata/goodzilla'
+  add_group 'Models', 'lib/gooddata/models'
+end
