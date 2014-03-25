a27aa0bc5f33bf6bd7c888d0f2c251195162e6c3
korczis <korczis@gmail.com>
2014-03-25 16:03:25 +0100

    Unit test of auth command

--- lib/gooddata/commands/auth.rb
+++ lib/gooddata/commands/auth.rb
@@ -63,14 +63,23 @@ module GoodData::Command
       end
 
       # Read credentials
-      def read_credentials
-        if File.exists?(credentials_file) then
-          config = File.read(credentials_file)
+      def read_credentials(credentials_file_path = credentials_file)
+        if File.exists?(credentials_file_path) then
+          config = File.read(credentials_file_path)
           JSON.parser.new(config, :symbolize_names => true).parse
         else
           {}
         end
       end
+
+      # Writes credentials
+      def write_credentials(credentials, credentials_file_path = credentials_file)
+        File.open(credentials_file_path, 'w', 0600) do |f|
+          f.puts JSON.pretty_generate(credentials)
+        end
+        credentials
+      end
+
 
       # Ask for credentials
       def ask_for_credentials
@@ -83,27 +92,25 @@ module GoodData::Command
       end
 
       # Ask for credentials and store them
-      def store
+      def store(credentials_file_path = credentials_file)
         credentials = ask_for_credentials
 
-        ovewrite = if File.exist?(credentials_file)
+        ovewrite = if File.exist?(credentials_file_path)
                      GoodData::CLI.terminal.ask("Overwrite existing stored credentials (y/n)")
                    else
                      'y'
                    end
 
         if ovewrite == 'y'
-          File.open(credentials_file, 'w', 0600) do |f|
-            f.puts JSON.pretty_generate(credentials)
-          end
+          write_credentials(credentials, credentials_file_path)
         else
           puts 'Aborting...'
         end
       end
 
       # Delete stored credentials
-      def unstore
-        FileUtils.rm_f(credentials_file)
+      def unstore(credentials_file_path = credentials_file)
+        FileUtils.rm_f(credentials_file_path)
       end
     end
   end
--- spec/unit/commands/command_auth_spec.rb
+++ spec/unit/commands/command_auth_spec.rb
@@ -8,6 +8,20 @@ require 'gooddata/commands/auth'
 describe GoodData::Command::Auth do
   ORIG_TERMINAL = GoodData::CLI::DEFAULT_TERMINAL
 
+  DEFAULT_CREDENTIALS = {
+    :email => 'joedoe@example.com',
+    :password => 'secretPassword',
+    :token => 't0k3n1sk0',
+  }
+
+  DEFAULT_CREDENTIALS_OVER = {
+    :email => 'pepa@depo.com',
+    :password => 'lokomotiva',
+    :token => 'briketa',
+  }
+  
+  DEFAULT_CREDENTIALS_TEMP_FILE_NAME = 'credentials'
+
   before(:all) do
     @input = StringIO.new
     @output = StringIO.new
@@ -73,33 +87,102 @@ describe GoodData::Command::Auth do
   end
 
   describe "#ask_for_credentials" do
-    credentials = {
-      :email => 'joedoe@example.com',
-      :password => 'secretPassword',
-      :token => 't0k3n1sk0',
-    }
-
     it 'Interactively asks user for crendentials' do
-      # pending("Mock STDIO")
-
-      @input << credentials[:email] << "\n"
-      @input << credentials[:password] << "\n"
-      @input << credentials[:token] << "\n"
+      @input.string = ''
+      @input << DEFAULT_CREDENTIALS[:email] << "\n"
+      @input << DEFAULT_CREDENTIALS[:password] << "\n"
+      @input << DEFAULT_CREDENTIALS[:token] << "\n"
       @input.rewind
 
       GoodData::Command::Auth.ask_for_credentials
     end
   end
 
+  describe "#read_credentials" do
+    it 'Reads credentials from default file if no path specified' do
+      GoodData::Command::Auth.read_credentials
+    end
+
+    it 'Reads credentials from file specified' do
+      temp_path = Tempfile.new(DEFAULT_CREDENTIALS_TEMP_FILE_NAME).path
+
+      GoodData::Command::Auth.write_credentials(DEFAULT_CREDENTIALS, temp_path)
+
+      result = GoodData::Command::Auth.read_credentials(temp_path)
+      GoodData::Command::Auth.unstore(temp_path)
+
+      result.should == DEFAULT_CREDENTIALS
+    end
+
+    it 'Returns empty hash if invalid path specified' do
+      expect = {}
+      result = GoodData::Command::Auth.read_credentials('/some/invalid/path')
+      result.should == expect
+    end
+  end
+
+  describe "#write_credentials" do
+    it 'Writes credentials' do
+      temp_path = Tempfile.new(DEFAULT_CREDENTIALS_TEMP_FILE_NAME).path
+
+      result = GoodData::Command::Auth.write_credentials(DEFAULT_CREDENTIALS, temp_path)
+      GoodData::Command::Auth.unstore(temp_path)
+
+      result.should == DEFAULT_CREDENTIALS
+    end
+  end
+
   describe "#store" do
     it 'Stores credentials' do
-      pending("Mock STDIO")
+      @input.string = ''
+      @input << DEFAULT_CREDENTIALS[:email] << "\n"
+      @input << DEFAULT_CREDENTIALS[:password] << "\n"
+      @input << DEFAULT_CREDENTIALS[:token] << "\n"
+      @input << 'y' << "\n"
+      @input.rewind
+
+      temp_path = Tempfile.new(DEFAULT_CREDENTIALS_TEMP_FILE_NAME).path
+      GoodData::Command::Auth.unstore(temp_path)
+      GoodData::Command::Auth.store(temp_path)
+    end
+
+    it 'Overwrites credentials if confirmed' do
+      @input.string = ''
+      @input << DEFAULT_CREDENTIALS[:email] << "\n"
+      @input << DEFAULT_CREDENTIALS[:password] << "\n"
+      @input << DEFAULT_CREDENTIALS[:token] << "\n"
+      @input << 'y' << "\n"
+      @input.rewind
+
+      temp_path = Tempfile.new(DEFAULT_CREDENTIALS_TEMP_FILE_NAME).path
+      GoodData::Command::Auth.write_credentials(DEFAULT_CREDENTIALS, temp_path)
+
+      GoodData::Command::Auth.store(temp_path)
+    end
+
+    it 'Do not overwrites credentials if not confirmed' do
+      @input.string = ''
+      @input << DEFAULT_CREDENTIALS_OVER[:email] << "\n"
+      @input << DEFAULT_CREDENTIALS_OVER[:password] << "\n"
+      @input << DEFAULT_CREDENTIALS_OVER[:token] << "\n"
+      @input << 'n' << "\n"
+      @input.rewind
+
+      temp_path = Tempfile.new(DEFAULT_CREDENTIALS_TEMP_FILE_NAME).path
+      GoodData::Command::Auth.write_credentials(DEFAULT_CREDENTIALS, temp_path)
+
+      GoodData::Command::Auth.store(temp_path)
+      result = GoodData::Command::Auth.read_credentials(temp_path)
+
+      result.should == DEFAULT_CREDENTIALS
     end
   end
 
   describe "#unstore" do
     it 'Removes stored credentials' do
-      pending("Mock fileutils")
+      temp_path = Tempfile.new(DEFAULT_CREDENTIALS_TEMP_FILE_NAME).path
+      GoodData::Command::Auth.write_credentials(DEFAULT_CREDENTIALS, temp_path)
+      GoodData::Command::Auth.unstore(temp_path)
     end
   end
 end
