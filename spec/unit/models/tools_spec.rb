4bdc44219f6f734c721ea5ff133e05c8f69472fc
korczis <korczis@gmail.com>
2014-03-31 16:36:46 +0200

    Rename to match lib structure

--- spec/unit/models/tools_spec.rb
+++ spec/unit/models/tools_spec.rb
@@ -4,29 +4,23 @@ require 'gooddata/client'
 require 'gooddata/models/model'
 
 describe GoodData::Model do
-  COLUMNS = [
-    {:type =>:anchor, :name =>'cp', 'title' => 'CP', 'folder' => 'test'},
-    {:type =>:attribute, :name =>'a1', 'title' => 'A1', 'folder' => 'test'},
-    {:type =>:attribute, :name =>'a2', 'title' => 'A2', 'folder' => 'test'},
-    {:type =>:date, :name =>'event', 'title' => 'Event', 'folder' => 'test'},
-    {:type =>:fact, :name =>'f1', 'title' => 'F1', 'folder' => 'test'},
-    {:type =>:fact, :name =>'f2', 'title' => 'F2', 'folder' => 'test'},
-  ]
-  SCHEMA = GoodData::Model::Schema.new :name => 'test', :title => 'test', :columns => COLUMNS
-
   before(:all) do
-    GoodData::connect
+    ConnectionHelper.create_default_connection
+  end
+
+  after(:all) do
+    GoodData.disconnect
   end
 
   it 'generate identifiers starting with letters and without ugly characters' do
     pending('Research what is desired behavior')
 
     expect = 'fact.test.blah'
-    result = GoodData::Model::Fact.new({:name =>'blah'}, SCHEMA).identifier
+    result = GoodData::Model::Fact.new({:name =>'blah'}, SchemaHelper::SCHEMA).identifier
     result.should == expect
 
     expect = 'attr.test.blah'
-    result = GoodData::Model::Attribute.new({:name =>'1_2_3 blah'}, SCHEMA).identifier
+    result = GoodData::Model::Attribute.new({:name =>'1_2_3 blah'}, SchemaHelper::SCHEMA).identifier
     result.should == expect
 
     expect = 'dim.blaz'
@@ -38,7 +32,7 @@ describe GoodData::Model do
     pending "Throws 400 - Bad request now"
 
     project = GoodData::Project.create :title => "gooddata-ruby test #{Time.new.to_i}"
-    objects = project.add_dataset 'Mrkev', COLUMNS
+    objects = project.add_dataset 'Mrkev', SchemaHelper::COLUMNS
 
     uris = objects['uris']
     uris[0].should == "#{project.md['obj']}/1"
@@ -52,7 +46,7 @@ describe GoodData::Model do
 
     project = GoodData::Project.create :title => "gooddata-ruby test #{Time.new.to_i}"
     GoodData.use project
-    objects = GoodData::Model.add_dataset 'Mrkev', COLUMNS
+    objects = GoodData::Model.add_dataset 'Mrkev', SchemaHelper::COLUMNS
 
     uris = objects['uris']
     uris[0].should == "#{project.md['obj']}/1"
@@ -75,7 +69,7 @@ describe GoodData::Model do
     GoodData.use project
 
     # create a similar data set but without the connection point column
-    cols_no_cp = COLUMNS.select { |c| c['type'] != 'CONNECTION_POINT' }
+    cols_no_cp = SchemaHelper::COLUMNS.select { |c| c['type'] != 'CONNECTION_POINT' }
     objects = GoodData::Model.add_dataset 'No CP', cols_no_cp
     uris = objects['uris']
 
