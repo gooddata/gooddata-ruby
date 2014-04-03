fcc86fb4dbb3da0e39c33d3d3c501c35e3d38c97
korczis <korczis@gmail.com>
2014-04-03 16:08:28 +0200

    Pass argument to scaffold brick and scaffold project

--- spec/unit/cli/commands/cmd_scaffold_spec.rb
+++ spec/unit/cli/commands/cmd_scaffold_spec.rb
@@ -1,8 +1,11 @@
-# encoding: UTF-8
+require 'fileutils'
 
 require 'gooddata/cli/cli'
 
 describe GoodData::CLI do
+  TEST_PROJECT_NAME = 'test-project'
+  TEST_BRICK_NAME = 'test-brick'
+
   it "Has working 'scaffold' command" do
     args = %w(scaffold)
 
@@ -19,5 +22,27 @@ describe GoodData::CLI do
     args = %w(scaffold project)
 
     run_cli(args)
+  end
+
+  it "Has working 'scaffold brick <brick-name>' command" do
+    args = [
+      'scaffold',
+      'brick',
+      TEST_BRICK_NAME
+    ]
+
+    run_cli(args)
+    FileUtils.rm_rf(TEST_BRICK_NAME)
+  end
+
+  it "Has working 'scaffold project <project-name>' command" do
+    args = [
+      'scaffold',
+      'project',
+      TEST_PROJECT_NAME
+    ]
+
+    run_cli(args)
+    FileUtils.rm_rf(TEST_PROJECT_NAME)
   end
 end
