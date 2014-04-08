983982e184926521b4b4ddc3bca1009395b34e3f
korczis <korczis@gmail.com>
2014-04-08 12:10:42 +0200

    new cli command: role list

---
+++ lib/gooddata/commands/role.rb
@@ -0,0 +1,24 @@
+# encoding: UTF-8
+
+require_relative '../core/core'
+
+module GoodData::Command
+  class Role
+    class << self
+      def list(pid)
+        roles_response = GoodData.get("/gdc/projects/#{pid}/roles")
+
+        roles = {}
+        roles_response["projectRoles"]["roles"].each do |role_uri|
+          r = GoodData.get(role_uri)
+          identifier = r["projectRole"]["meta"]["identifier"]
+          roles[identifier] = {
+            :user_uri => r["projectRole"]["links"]["roleUsers"],
+            :uri => role_uri
+          }
+        end
+        roles
+      end
+    end
+  end
+end
--- lib/gooddata/commands/user.rb
+++ lib/gooddata/commands/user.rb
@@ -34,6 +34,21 @@ module GoodData::Command
         end
         users
       end
+
+      def get_roles(pid)
+        roles_response = GoodData.get("/gdc/projects/#{pid}/roles")
+
+        roles = {}
+        roles_response["projectRoles"]["roles"].each do |role_uri|
+          r = GoodData.get(role_uri)
+          identifier = r["projectRole"]["meta"]["identifier"]
+          roles[identifier] = {
+            :user_uri => r["projectRole"]["links"]["roleUsers"],
+            :uri => role_uri
+          }
+        end
+        roles
+      end
     end
   end
 end
