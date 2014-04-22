be75cb2ea20e935648ae139c50e9a9a0ba892156
Tomas Svarovsky <svarovsky.tomas@gmail.com>
2014-04-22 14:06:00 -0700

    Added a way to create a count metrics from attributes directly. Returns a metric.

--- lib/gooddata/models/metadata/attribute.rb
+++ lib/gooddata/models/metadata/attribute.rb
@@ -29,10 +29,16 @@ module GoodData
     end
 
     def create_metric(options={})
-      a_type = options[:type] || :sum
+      an_attribute = options[:attribute]
+      a_type = options[:type] || :count
       fail "Suggested aggreagtion function (#{a_type}) does not exist for base metric created out of attribute. You can use only one of #{ATTRIBUTE_BASE_AGGREGATIONS.map {|x| ":" + x.to_s}.join(',')}" unless ATTRIBUTE_BASE_AGGREGATIONS.include?(a_type)
       a_title = options[:title] || "#{a_type} of #{title}"
-      Metric.xcreate(:expression => "SELECT #{a_type.to_s.upcase}(![#{identifier}])", :title => a_title)
+      if an_attribute
+        an_attribute = Attribute[an_attribute] if an_attribute.is_a?(String)
+        Metric.xcreate(:expression => "SELECT #{a_type.to_s.upcase}(![#{identifier}], ![#{an_attribute.identifier}])", :title => a_title)
+      else
+        Metric.xcreate(:expression => "SELECT #{a_type.to_s.upcase}(![#{identifier}])", :title => a_title)
+      end
     end
 
   end
