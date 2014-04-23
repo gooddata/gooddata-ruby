80072a40066ec172ceb76c87f86e4ad16b5dc37f
korczis <korczis@gmail.com>
2014-04-23 15:33:46 -0700

    Few more tests

--- spec/unit/models/project_spec.rb
+++ spec/unit/models/project_spec.rb
@@ -13,7 +13,23 @@ describe GoodData::Project do
 
   describe '#all' do
     it 'Returns all projects' do
+      # TODO: This should not throw error
       expect { GoodData::Project.all }.to raise_error(RestClient::Forbidden)
+    end
+  end
+
+  describe '#[]' do
+    it 'Accepts :all parameter' do
+      # TODO: This should not throw error
+      expect { GoodData::Project[:all] }.to raise_error(RestClient::Forbidden)
+    end
+  end
+
+  describe '#get_roles' do
+    it 'Returns array' do
+      proj = GoodData::Project['la84vcyhrq8jwbu4wpipw66q2sqeb923']
+      roles = proj.get_roles
+      expect(roles).to be_instance_of(Array)
     end
   end
 end
