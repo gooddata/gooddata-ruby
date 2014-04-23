c91ad813ee85cd208a2b7706852b13de939a5e00
korczis <korczis@gmail.com>
2014-04-23 15:59:57 -0700

    Fixed tests (???)

--- spec/unit/models/project_spec.rb
+++ spec/unit/models/project_spec.rb
@@ -3,6 +3,13 @@
 require 'gooddata'
 
 describe GoodData::Project do
+  DEFAULT_PID = 'la84vcyhrq8jwbu4wpipw66q2sqeb923'
+
+  def get_default_proj
+    GoodData::Project[DEFAULT_PID]
+  end
+
+
   before(:each) do
     ConnectionHelper::create_default_connection
   end
@@ -13,23 +20,36 @@ describe GoodData::Project do
 
   describe '#all' do
     it 'Returns all projects' do
-      # TODO: This should not throw error
-      expect { GoodData::Project.all }.to raise_error(RestClient::Forbidden)
+      GoodData::Project.all
     end
   end
 
   describe '#[]' do
     it 'Accepts :all parameter' do
-      # TODO: This should not throw error
-      expect { GoodData::Project[:all] }.to raise_error(RestClient::Forbidden)
+      GoodData::Project[:all]
     end
   end
 
   describe '#get_roles' do
     it 'Returns array' do
-      proj = GoodData::Project['la84vcyhrq8jwbu4wpipw66q2sqeb923']
+      proj = get_default_proj
       roles = proj.get_roles
       expect(roles).to be_instance_of(Array)
     end
   end
+
+  describe '#get_role_by_identifier' do
+    it 'Looks up for role by identifier' do
+      proj = get_default_proj
+      proj.get_role_by_identifier('admin')
+    end
+  end
+
+  describe '#get_role_by_title' do
+    it 'Looks up for role by title' do
+      proj = get_default_proj
+      proj.get_role_by_title('admin')
+    end
+  end
+
 end
