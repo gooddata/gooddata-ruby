9bb9eb4e823f2ccad5ae3551ac3c27fafc3b3fe7
Tomas Korcak <korczis@gmail.com>
2014-04-30 12:38:23 -0700

    First steps with schedules

---
+++ spec/unit/models/schedule_spec.rb
@@ -0,0 +1,23 @@
+require 'gooddata/models/schedule'
+
+describe GoodData::Schedule do
+  before(:each) do
+    ConnectionHelper.create_default_connection
+  end
+
+  after(:each) do
+    GoodData.disconnect
+  end
+
+  it 'Will get schedules for process' do
+    # GoodData::connect('patrick.mcconlogue@gooddata.com', 'seeme1234')
+
+    GoodData.project = 'tk6192gsnav58crp6o1ahsmtuniq8khb'
+
+    proj = GoodData.project
+    proc = proj.processes.first
+
+    sched = proc.schedules.first
+    sched.execute
+  end
+end
