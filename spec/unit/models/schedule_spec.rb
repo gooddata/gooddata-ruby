d1dbb8a49423754073c2f74a648abf7e48ac1496
Tomas Korcak <korczis@gmail.com>
2014-04-30 12:46:24 -0700

    Remove commented stuff

--- spec/unit/models/schedule_spec.rb
+++ spec/unit/models/schedule_spec.rb
@@ -10,8 +10,6 @@ describe GoodData::Schedule do
   end
 
   it 'Will get schedules for process' do
-    # GoodData::connect('patrick.mcconlogue@gooddata.com', 'seeme1234')
-
     GoodData.project = 'tk6192gsnav58crp6o1ahsmtuniq8khb'
 
     proj = GoodData.project
