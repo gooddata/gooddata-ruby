86ef98d2fd7f3ab4b07ee632067ee78edc375180
Patrick McConlogue <patrick.mcconlogue@gooddata.com>
2014-04-30 17:35:36 -0700

    Added update_json object to insure the response from the server was not included in the update PUT

--- lib/gooddata/models/schedule.rb
+++ lib/gooddata/models/schedule.rb
@@ -76,8 +76,65 @@ module GoodData
       @json['schedule']['state']
     end
 
-    def graph
-      @json['schedule']['params']['GRAPH']
+    def params
+      @json['schedule']['params']
+    end
+
+    def timezone
+      @json['schedule']['timezone']
+    end
+
+    def timezone=(new_timezone)
+      @json['schedule']['timezone'] = new_timezone
+      @dirty = true
+    end
+
+    def type=(new_type)
+      @json['schedule']['type'] = new_type
+      @dirty = true
+    end
+
+    def cron=(new_cron)
+      @json['schedule']['cron'] = new_cron
+      @dirty = true
+    end
+
+    def process_id=(new_project_id)
+      @json['schedule']['params']['PROCESS_ID'] = new_project_id
+      @dirty = true
+    end
+
+    def executable=(new_executable)
+      @json['schedule']['params']['EXECUTABLE'] = new_executable
+      @dirty = true
+    end
+
+    def params=(new_param)
+      @json['schedule']['params'].merge!(new_param)
+      @dirty = true
+    end
+
+    def hiddenParams=(new_hidden_param)
+      @json['schedule']['hiddenParams'].merge!(new_hidden_param)
+      @dirty = true
+    end
+
+    def save
+      if @dirty
+        update_json = {
+            'schedule' => {
+                'type' => @json['schedule']['type'],
+                'timezone' => @json['schedule']['timezone'],
+                'cron' => @json['schedule']['cron'],
+                'params' => @json['schedule']['params'],
+                'hiddenParams'=> @json['schedule']['hiddenParams']
+            }
+        }
+        res = GoodData.put self.uri, update_json
+
+        @json = res
+        @dirty = false
+      end
     end
 
     def uri
--- spec/unit/models/schedule_spec.rb
+++ spec/unit/models/schedule_spec.rb
@@ -1,8 +1,6 @@
 require 'gooddata/models/schedule'
 
 describe GoodData::Schedule do
-  PROJECT_ID = 'tk6192gsnav58crp6o1ahsmtuniq8khb'
-
   before(:each) do
     ConnectionHelper.create_default_connection
   end
@@ -11,8 +9,8 @@ describe GoodData::Schedule do
     GoodData.disconnect
   end
 
-  it 'Can delete created schedule' do
-    GoodData.project = PROJECT_ID
+  it 'Will get schedules for process' do
+    GoodData.project = 'tk6192gsnav58crp6o1ahsmtuniq8khb'
 
     proj = GoodData.project
 
@@ -22,8 +20,17 @@ describe GoodData::Schedule do
     }
 
     sched = GoodData::Schedule.create('f12975d2-5958-4248-9c3d-4c8f2e1f067d', '0 15 27 7 *', "#{proj.title}/graph/graph.grf", data)
-    pp sched
 
-    sched.delete
+    sched.params = {
+        'name' => 'patrick'
+    }
+
+    pp sched.params
+
+    sched.save
+
+    pp sched
+    # pp sched
+    # sched.delete
   end
 end
