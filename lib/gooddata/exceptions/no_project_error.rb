ab31fcbfe874487375f666226b8b2bf6844d3671
Tomas Korcak <korczis@gmail.com>
2014-05-12 16:02:16 -0700

    Support for message, default one and explicitly set one

--- lib/gooddata/exceptions/no_project_error.rb
+++ lib/gooddata/exceptions/no_project_error.rb
@@ -2,5 +2,10 @@
 
 module GoodData
   class NoProjectError < RuntimeError
+    DEFAULT_MSG = 'You have to provide "project_id". You can either provide it through -p flag or even better way is to fill it in in your Goodfile under key "project_id". If you just started a project you have to create it first. One way might be through "gooddata project build"'
+
+    def initialize(msg = DEFAULT_MSG)
+      super(msg)
+    end
   end
 end
