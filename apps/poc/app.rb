b7fc4c6a8a197165a4f842800ca648e2d1ba7317
Tomas Korcak <korczis@gmail.com>
2014-05-19 23:31:11 -0700

    More comments

--- apps/poc/app.rb
+++ apps/poc/app.rb
@@ -9,14 +9,29 @@ module GoodData
       DEFAULT_PASSWORD = 'jindrisska'
 
       def main(argv = ARGV)
+        # Connect using username and password
         client = GoodData::Rest::Client.connect(DEFAULT_USERNAME, DEFAULT_PASSWORD)
 
-       #  projects = client.resource(GoodData::Project)
+        # Show the connection result
+        pp client
+
+        # List projects
+        # projects = client.all(GoodData::Project)
+        #
+        # Show projects listing result
+        # pp projects
+
+        # Find by resource class
+        # find_result = client.find(GoodData::Project, 'id')
+
+        # Find resource by arguments
+        # find_result = client.find_by(GoodData::Project, {:title => 'GoodSales'})
       end
     end
   end
 end
 
+# Run this ruby file as standalone ruby script if not included programmaticaly
 if __FILE__ == $0
   app = GoodData::Apps::PocApp.new
   app.main
