84cbace2980e5cb456f5f53efa3ebe2e71633e4a
Tomas Korcak <korczis@gmail.com>
2014-05-19 23:37:38 -0700

    GoodData::Rest::Resource subclass

--- lib/gooddata/models/project.rb
+++ lib/gooddata/models/project.rb
@@ -7,10 +7,10 @@ require_relative 'process'
 require_relative '../exceptions/no_project_error'
 require_relative 'project_role'
 
-require_relative '../rest/object'
+require_relative '../rest/resource'
 
 module GoodData
-  class Project < GoodData::Rest::Object
+  class Project < GoodData::Rest::Resource
     USERSPROJECTS_PATH = '/gdc/account/profile/%s/projects'
     PROJECTS_PATH = '/gdc/projects'
     PROJECT_PATH = '/gdc/projects/%s'
