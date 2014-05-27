4db5f3ad3d521fd2a24316e174ff3c9376a520fa
Tomas Korcak <korczis@gmail.com>
2014-05-27 15:39:03 -0700

    Fixed few more errors

--- lib/gooddata/rest/client.rb
+++ lib/gooddata/rest/client.rb
@@ -167,6 +167,20 @@ module GoodData
       def post(uri, data, opts = {})
         @connection.post uri, data, opts
       end
+
+      def retryable(options = {}, &block)
+        opts = {:tries => 1, :on => Exception}.merge(options)
+
+        retry_exception, retries = opts[:on], opts[:tries]
+
+        begin
+          return yield
+        rescue retry_exception
+          retry if (retries -= 1) > 0
+        end
+
+        yield
+      end
     end
   end
 end
