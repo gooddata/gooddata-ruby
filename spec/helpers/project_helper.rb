8829a806692ccb5a186c0dad4c15130f87d6bb70
Tomas Korcak <korczis@gmail.com>
2014-07-21 11:57:49 -0700

    Support for deleting projects

--- spec/helpers/project_helper.rb
+++ spec/helpers/project_helper.rb
@@ -10,7 +10,22 @@ module ProjectHelper
   PROJECT_ID ||= 'ghbpozicaidf1b9s0ohsa6msu7792c1k'
   PROJECT_URL ||= "/gdc/projects/#{PROJECT_ID}"
 
-  def self.get_default_project
-    GoodData::Project[PROJECT_ID]
+  class << self
+    def get_default_project
+      GoodData::Project[PROJECT_ID]
+    end
+
+    def delete_all_projects
+      projects = GoodData::Project[:all]
+
+      user = GoodData.user
+
+      while !projects.empty?
+        project = projects.shift
+        should_delete =  project.author == user.uri
+        project.delete if should_delete
+      end
+
+    end
   end
 end
--- spec/helpers/report_helper.rb
+++ spec/helpers/report_helper.rb
@@ -15,6 +15,19 @@ module ReportHelper
 
     # TODO: This function is responsible for creating new report which will be used for tests
     def create_default_reports
+      project = ProjectHelper.get_default_project
+      metric = MetricHelper.default_metric
+
+      GoodData::ReportDefinitionBuilder.chart_types.each do |chart_type|
+        title = "Report #{metric.title} - #{chart_type}"
+        definition = GoodData::ReportDefinitionBuilder.create(metric, :title => title, :type => chart_type)
+        definition.save(project)
+
+        report = GoodData::ReportBuilder.create(definition)
+        report.save(project)
+
+        @@REPORTS << report
+      end
     end
 
     def remove_default_reports
