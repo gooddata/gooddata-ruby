929b7ab8ff0b33752f7d253e22c6c80966251b86
Tomas Korcak <tomas.korcak@gooddata.com>
2014-07-21 11:57:30 -0700

    Update project_helper.rb

--- spec/helpers/project_helper.rb
+++ spec/helpers/project_helper.rb
@@ -7,7 +7,7 @@ require 'multi_json'
 require 'gooddata/models/models'
 
 module ProjectHelper
-  PROJECT_ID = 'ghbpozicaidf1b9s0ohsa6msu7792c1k'
+  PROJECT_ID = 'hj7lx11gtcdh8z0q3zgpofz3qcnzkt7h'
   PROJECT_URL = "/gdc/projects/#{PROJECT_ID}"
 
   def self.get_default_project
