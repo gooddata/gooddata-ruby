6424299ce2c4d2271d2141249f44f6ce5ccdc866
Tomas Korcak <korczis@gmail.com>
2014-07-23 13:29:39 -0700

    Fixed metric identifier

--- spec/helpers/metric_helper.rb
+++ spec/helpers/metric_helper.rb
@@ -7,7 +7,7 @@ require_relative 'project_helper'
 
 module MetricHelper
   DEFAULT_METRIC_NAME ||= 'Lines changed [Sum]'
-  DEFAULT_METRIC_IDENTIFIER ||= '"buRRk4Y4by0w"'
+  DEFAULT_METRIC_IDENTIFIER ||= 'buRRk4Y4by0w'
   DEFAULT_METRIC_OBJ_ID ||= 241
   DEFAULT_METRIC_URI ||= "/gdc/md/#{ProjectHelper::PROJECT_ID}/obj/#{DEFAULT_METRIC_OBJ_ID}"
 
