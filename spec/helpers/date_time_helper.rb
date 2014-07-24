17aa81fe0b58a7a17678f1f04d6ae70bcc8c3f2f
Tomas Korcak <korczis@gmail.com>
2014-07-24 15:02:54 -0700

    Timestamp with random number

--- spec/helpers/date_time_helper.rb
+++ spec/helpers/date_time_helper.rb
@@ -6,5 +6,5 @@ require 'gooddata/models/models'
 require_relative 'project_helper'
 
 module DateTimeHelper
-  TIMESTAMP ||= Time.new.strftime('%Y%m%d%H%M%S')
+  TIMESTAMP ||= Time.new.strftime('%Y%m%d%H%M%S') + ".#{rand(1e6)}"
 end
