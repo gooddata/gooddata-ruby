21f37741c892a2d9dd067acfae696d24c1b5b23e
Tomas Korcak <korczis@gmail.com>
2014-07-31 17:08:25 -0700

    Merge with upstream master

--- spec/helpers/project_helper.rb
+++ spec/helpers/project_helper.rb
@@ -7,7 +7,7 @@ require 'multi_json'
 require 'gooddata/models/models'
 
 module ProjectHelper
-  PROJECT_ID = 'ghbpozicaidf1b9s0ohsa6msu7792c1k'
+  PROJECT_ID = 'we1vvh4il93r0927r809i3agif50d7iz'
   PROJECT_URL = "/gdc/projects/#{PROJECT_ID}"
 
   def self.get_default_project
