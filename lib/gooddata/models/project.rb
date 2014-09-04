e9a3d86825a5ee7c9ef13193a754f6b65afc93e1
Tomas Korcak <korczis@gmail.com>
2014-09-04 12:03:32 -0700

    Missing require

--- lib/gooddata/models/project.rb
+++ lib/gooddata/models/project.rb
@@ -10,6 +10,8 @@ require 'zip'
 require_relative '../exceptions/no_project_error'
 
 require_relative '../rest/resource'
+require_relative '../mixins/author'
+require_relative '../mixins/contributor'
 require_relative '../mixins/rest_resource'
 
 require_relative 'process'
