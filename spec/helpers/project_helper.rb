6d47a25eedd5d127f48a0af097386137dee68430
Tomas Korcak <korczis@gmail.com>
2015-01-09 12:38:31 +0100

    Delete old projects

--- spec/helpers/project_helper.rb
+++ spec/helpers/project_helper.rb
@@ -15,4 +15,18 @@ module ProjectHelper
   def self.get_default_project(opts = { :client => GoodData.connection })
     GoodData::Project[PROJECT_ID, opts]
   end
+
+  def self.delete_old_projects(opts = {:client => GoodData.connection})
+    projects = opts[:client].projects
+    projects.each do |project|
+      next if project.json['project']['meta']['author'] != client.user.uri
+      next if project.pid == 'we1vvh4il93r0927r809i3agif50d7iz'
+      begin
+        puts "Deleting project #{project.title}"
+        project.delete
+      rescue e
+        puts 'ERROR: ' + e.to_s
+      end
+    end
+  end
 end
