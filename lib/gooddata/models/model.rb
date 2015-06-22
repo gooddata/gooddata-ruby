80d55d712668e7d0fb269ffd41863027a58127a6
Tomas Korcak <korczis@gmail.com>
2015-06-22 11:05:42 +0200

    Add all attribute types

--- lib/gooddata/models/model.rb
+++ lib/gooddata/models/model.rb
@@ -19,7 +19,68 @@ require 'zip'
 #
 module GoodData
   module Model
-    GD_TYPES = %w(GDC.link GDC.text GDC.geo GDC.time)
+    # See https://confluence.intgdc.com/display/plat/Catalog+of+Attribute+Types
+    GD_TYPES = [
+      # Common Types
+      'GDC.link',
+      'GDC.text',
+      'GDC.time',
+
+      # Common Date Attribute Types
+      'GDC.time.year',
+      'GDC.time.quarter',
+      'GDC.time.month',
+      'GDC.time.week',
+      'GDC.time.date',
+
+      # Specific Date Attribute Types
+      'GDC.time.day_in_euweek',
+      'GDC.time.day_in_week',
+      'GDC.time.day_in_month',
+      'GDC.time.day_in_quarter',
+      'GDC.time.day_in_year',
+      'GDC.time.euweek_in_quarter',
+      'GDC.time.week_in_quarter',
+      'GDC.time.euweek_in_year',
+      'GDC.time.week_in_year',
+      'GDC.time.month_in_quarter',
+      'GDC.time.month_in_year',
+      'GDC.time.quarter_in_year',
+
+      # Legacy Date Attribute Types - Possibly Obsolete
+      'GDC.time.dayOfWeek',
+      'GDC.time.dayOfMonth',
+      'GDC.time.dayOfQuarter',
+      'GDC.time.dayOfYear',
+      'GDC.time.weekOfYear',
+      'GDC.time.monthOfYear',
+      'GDC.time.quarterOfYear',
+
+      # Types for Geo
+      'GDC.geo.pin',                 # Geo pushpin
+      'GDC.geo.ausstates.name',      # Australia States (Name)
+      'GDC.geo.ausstates.code',      # Australia States (ISO code)
+      'GDC.geo.usstates.name',       # US States (Name)
+      'GDC.geo.usstates.geo_id',     # US States (US Census ID)
+      'GDC.geo.usstates.code',       # US States (2-letter code)
+      'GDC.geo.uscounties.geo_id',   # US Counties (US Census ID)
+      'GDC.geo.worldcountries.name', # World countries (Name)
+      'GDC.geo.worldcountries.iso2', # World countries (ISO a2)
+      'GDC.geo.worldcountries.iso3', # World countries (ISO a3)
+      'GDC.geo.czdistricts.name',    #	Czech Districts (Name)
+      'GDC.geo.czdistricts.name_no_diacritics', #	Czech Districts
+      'GDC.geo.czdistricts.nuts4',   # Czech Districts (NUTS 4)
+      'GDC.geo.czdistricts.knok',    # Czech Districts (KNOK)
+
+      # Day Display Forms
+      'GDC.time.day',              # yyyy-MM-dd
+      'GDC.time.day_us',           # MM/dd/yyyy
+      'GDC.time.day_eu',           # dd/MM/yyyy
+      'GDC.time.day_iso',          # dd-MM-yyyy
+      'GDC.time.day_us_long',      # EEE, MMM d, yyyy
+      'GDC.time.day_us_noleading', # M/d/yy
+    ]
+
     GD_DATA_TYPES = ['BIGINT', 'DOUBLE', 'INTEGER', 'INT', /^VARCHAR\(\d{1,3}\)$/i, /^DECIMAL\(\d{1,3},\s*\d{1,3}\)$/i]
 
     DEFAULT_FACT_DATATYPE = 'DECIMAL(12,2)'
