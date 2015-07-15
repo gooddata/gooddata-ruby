7cb9ce1255e893c09eb416aba46c07e4d563a1b3
Tomas Korcak <korczis@gmail.com>
2015-07-15 16:39:45 +0200

    [BIA-77] - Add support of GoodData PGP SSO to ruby SDK

--- lib/gooddata/connection.rb
+++ lib/gooddata/connection.rb
@@ -6,6 +6,11 @@ require_relative 'rest/rest'
 
 module GoodData
   class << self
+    DEFAULT_SSO_OPTIONS = {
+      :url => '/gdc/app/account/bootstrap',
+      :valid => 24 * 60 * 60
+    }
+
     # Returns the active GoodData connection earlier initialized via GoodData.connect call
     #
     # @see GoodData.connect
@@ -39,6 +44,40 @@ module GoodData
       raise e
     ensure
       disconnect
+    end
+
+    def sso_url(login, provider, opts = DEFAULT_SSO_OPTIONS)
+      opts = DEFAULT_SSO_OPTIONS.merge(opts)
+
+      ts = DateTime.now.strftime('%s').to_i + opts[:valid]
+      obj = {
+        'email' => login,
+        'validity' => ts
+      }
+
+      json_data = JSON.pretty_generate(obj) + "\n"
+
+      file_json = Tempfile.new('gooddata-sso-json')
+      file_json.write(json_data)
+
+      file_json.rewind
+      file_signed = Tempfile.new('gooddata-sso-signed')
+
+      cmd = "gpg --no-tty --armor --yes -u #{login} --output #{file_signed.path} --sign #{file_json.path}"
+      res = system(cmd)
+      fail 'Unable to sign json' unless res
+
+      file_signed.rewind
+      file_final = Tempfile.new('gooddata-sso-final')
+
+      cmd = "gpg --yes --no-tty --trust-model always --armor --output #{file_final.path} --encrypt --recipient security@gooddata.com #{file_signed.path}"
+      res = system(cmd)
+      fail 'Unable to encrypt json' unless res
+
+      file_final.rewind
+      final = file_final.read
+
+      "#{GoodData::Helpers::AuthHelper.read_server}/gdc/account/customerlogin?sessionId=#{CGI.escape(final)}&serverURL=#{CGI.escape(provider)}&targetURL=#{CGI.escape(opts[:url])}"
     end
   end
 end
