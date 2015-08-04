b96e417dfc054e6a66ada9746d43554e6b4c85f3
Tomas Korcak <korczis@gmail.com>
2015-08-04 21:46:26 +0200

    Added generated headers

--- examples.rb
+++ examples.rb
@@ -1,4 +1,8 @@
 # encoding: UTF-8
+#
+# Copyright (c) 2010-2015 GoodData Corporation. All rights reserved.
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
 
 # This file shows how the GoodData Ruby API could be used from
 # a client code
--- lib/gooddata/models/metadata/label.rb
+++ lib/gooddata/models/metadata/label.rb
@@ -1,4 +1,8 @@
 # encoding: UTF-8
+#
+# Copyright (c) 2010-2015 GoodData Corporation. All rights reserved.
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
 
 require_relative '../metadata'
 require_relative '../../mixins/is_label'
