9b2c4449057265b322d273b3935becff790b6865
Tomas Korcak <korczis@gmail.com>
2015-08-07 13:20:01 +0200

    [BIA-166] - Improve dashboards

---
+++ lib/gooddata/models/metadata/dashboard/filter_item.rb
@@ -0,0 +1,15 @@
+# encoding: UTF-8
+#
+# Copyright (c) 2010-2015 GoodData Corporation. All rights reserved.
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+require_relative 'dashboard_item'
+
+module GoodData
+  class FilterItem < DashboardItem
+    def initialize(tab, json)
+      super
+    end
+  end
+end
