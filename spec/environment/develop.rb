82bf8218e16213760b4f6ebc0472cd6c713c22d4
Tomas Korcak <korczis@gmail.com>
2015-10-26 13:58:34 -0700

    [BIA-184] - set_const

--- spec/environment/develop.rb
+++ spec/environment/develop.rb
@@ -7,25 +7,25 @@
 module GoodData
   module Environment
     module ConnectionHelper
-      DEFAULT_SERVER = 'https://staging3.intgdc.com'
-      DEFAULT_USER_URL = '/gdc/account/profile/a3700850b92a0a6c097e48369b5d226f'
-      STAGING_URI = 'https://staging3.intgdc.com/gdc/uploads/'
+      set_const :DEFAULT_SERVER, 'https://staging3.intgdc.com'
+      set_const :DEFAULT_USER_URL, '/gdc/account/profile/a3700850b92a0a6c097e48369b5d226f'
+      set_const :STAGING_URI, 'https://staging3.intgdc.com/gdc/uploads/'
     end
 
     module ProcessHelper
-      PROCESS_ID = 'e4369e27-a6c7-4782-b4cb-a06b2e2db326'
-      DEPLOY_NAME = 'graph/graph.grf'
+      set_const :PROCESS_ID, 'e4369e27-a6c7-4782-b4cb-a06b2e2db326'
+      set_const :DEPLOY_NAME, 'graph/graph.grf'
     end
 
     module ProjectHelper
-      PROJECT_ID = 'hq7ik4hpsaknrsvb07jz2adbci1iq5s8'
-      PROJECT_URL = "/gdc/projects/#{PROJECT_ID}"
-      PROJECT_TITLE = 'GoodTravis'
-      PROJECT_SUMMARY = 'No summary'
+      set_const :PROJECT_ID, 'hq7ik4hpsaknrsvb07jz2adbci1iq5s8'
+      set_const :PROJECT_URL, "/gdc/projects/#{PROJECT_ID}"
+      set_const :PROJECT_TITLE, 'GoodTravis'
+      set_const :PROJECT_SUMMARY, 'No summary'
     end
 
     module ScheduleHelper
-      SCHEDULE_ID = '55ed6757e4b0d165852ab308'
+      set_const :SCHEDULE_ID, '55ed6757e4b0d165852ab308'
     end
   end
 end
--- spec/environment/hotfix.rb
+++ spec/environment/hotfix.rb
@@ -7,15 +7,15 @@
 module GoodData
   module Environment
     module ConnectionHelper
-      DEFAULT_SERVER = 'https://staging2.intgdc.com'
-      DEFAULT_USER_URL = '/gdc/account/profile/d8b356b30c0c12d1b4b97f56d6706ef2'
+      set_const :DEFAULT_SERVER, 'https://staging2.intgdc.com'
+      set_const :DEFAULT_USER_URL, '/gdc/account/profile/d8b356b30c0c12d1b4b97f56d6706ef2'
     end
 
     module ProjectHelper
-      PROJECT_ID = 'i640il7dyatqmvak24zzr09ypt3ghqu2'
-      PROJECT_URL = "/gdc/projects/#{PROJECT_ID}"
-      PROJECT_TITLE = 'GoodTravis'
-      PROJECT_SUMMARY = 'No summary'
+      set_const :PROJECT_ID, 'i640il7dyatqmvak24zzr09ypt3ghqu2'
+      set_const :PROJECT_URL, "/gdc/projects/#{PROJECT_ID}"
+      set_const :PROJECT_TITLE, 'GoodTravis'
+      set_const :PROJECT_SUMMARY, 'No summary'
     end
   end
 end
