3b20bb483f634f30dca98457cfa230b33fa7985b
vhtien <tien.vo@gooddata.com>
2016-11-28 14:51:23 +0700

    BUGFIX: BIA-535 Fix Inconsistent / Obsolete environment files

--- spec/environment/staging.rb
+++ spec/environment/staging.rb
@@ -7,24 +7,21 @@
 module GoodData
   module Environment
     module ConnectionHelper
-      set_const :DEFAULT_SERVER, 'https://staging.getgooddata.com'
+      set_const :DEFAULT_SERVER, 'https://staging.intgdc.com'
       set_const :DEFAULT_USER_URL, '/gdc/account/profile/6ca71a392e15700a4db4537f441eba39'
+      set_const :STAGING_URI, 'https://ea-di.staging.intgdc.com/uploads/'
     end
 
     module ProcessHelper
-      set_const :PROCESS_ID, '94e7be67-5f68-405d-bdeb-93006d50482d'
-      set_const :DEPLOY_NAME, 'graph/graph.grf'
+      set_const :PROCESS_ID, 'd5de77aa-cf96-4018-bbaa-09a905330c8a'
     end
 
     module ProjectHelper
-      set_const :PROJECT_ID, 'k8rzngunca3t9dywmxhqzpgwlui3yg0m'
-      set_const :PROJECT_URL, "/gdc/projects/#{PROJECT_ID}"
-      set_const :PROJECT_TITLE, 'GoodTravis'
-      set_const :PROJECT_SUMMARY, 'No summary'
+      set_const :PROJECT_ID, 'nz708agdlxos81clatziwpe6o5hph2c8'
     end
 
     module ScheduleHelper
-      set_const :SCHEDULE_ID, '55953261e4b0a92792febe4e'
+      set_const :SCHEDULE_ID, '583c08f2e4b0479e793a161f'
     end
   end
 end
