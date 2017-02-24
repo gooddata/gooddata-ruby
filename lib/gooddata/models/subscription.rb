8d71f52827d314d85ff868a74a036030009ad8a3
Tomas Korcak <korczis@gmail.com>
2017-02-24 16:04:01 +0100

    Update subscription.rb

--- lib/gooddata/models/subscription.rb
+++ lib/gooddata/models/subscription.rb
@@ -4,6 +4,8 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
+require_relative '../mixins/rest_resource'
+
 module GoodData
   class Subscription < Rest::Resource
     SUBSCRIPTION_PATH = '/gdc/projects/%s/users/%s/subscriptions'
