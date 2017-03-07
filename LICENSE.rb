9261b507f0dbda812a2abbee5eb11995fdef07b7
vhtien <tien.vo@gooddata.com>
2017-03-07 15:54:54 +0700

    RELATED: TMA-290 Update the year to 2017 in License headers

--- LICENSE.rb
+++ LICENSE.rb
@@ -1,5 +1,5 @@
 # encoding: UTF-8
 #
-# Copyright (c) 2010-2015 GoodData Corporation. All rights reserved.
+# Copyright (c) 2010-2017 GoodData Corporation. All rights reserved.
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
