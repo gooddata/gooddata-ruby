372ac0cb1738ba67a3e73e5692504fd68181e424
panjan <jan.zdrahal@gooddata.com>
2017-03-14 19:43:18 +0100

    Review test environments config

--- spec/environment/environment.rb
+++ spec/environment/environment.rb
@@ -7,19 +7,31 @@
 module GoodData
   module Environment
     class << self
-      def load(env = (ENV['GD_ENV'] && ENV['GD_ENV'].split('/')[1]) || 'develop')
+      BRANCH_TO_ENVIRONMENT = {
+        develop: 'testing',
+        hotfix: 'production'
+      }
+
+      def load(env = ENV['GD_ENV'] || 'development')
         require_relative 'default'
+        env = branch_to_environment(env)
 
         puts "USING ENVIRONMENT: #{env}"
         begin
           require_relative env
         rescue
           puts "Unable to find environment '#{env}'"
-          require_relative 'develop'
+          require_relative 'development'
         end
 
         GoodData::Environment::ProjectHelper.set_const :PROJECT_URL, "/gdc/projects/#{GoodData::Environment::ProjectHelper::PROJECT_ID}"
         ENV['GD_SERVER'] = GoodData::Environment::ConnectionHelper::DEFAULT_SERVER
+      end
+
+      # Translates ci-infra branch to environment config name
+      # @param branch Name of the branch we are testing against
+      def branch_to_environment(branch)
+        BRANCH_TO_ENVIRONMENT[branch.to_sym] || branch
       end
     end
   end
