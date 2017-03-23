915db717a497dd1c1e343064a779b8055c41471a
Tomas Korcak <tomas.korcak@gooddata.com>
2017-03-23 13:28:52 +0100

    Fix middleware

--- lib/gooddata/bricks/middleware/middleware.rb
+++ lib/gooddata/bricks/middleware/middleware.rb
@@ -4,9 +4,16 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
-require 'pathname'
-
-base = Pathname(__FILE__).dirname.expand_path
-Dir.glob("#{base}/*_middleware.rb").each do |file|
-  require file
-end
+require_relative 'aws_middleware'
+require_relative 'base_middleware'
+require_relative 'bulk_salesforce_middleware'
+require_relative 'decode_params_middleware'
+require_relative 'dwh_middleware' if RUBY_PLATFORM == 'java'
+require_relative 'fs_download_middleware'
+require_relative 'gooddata_middleware'
+require_relative 'logger_middleware'
+require_relative 'params_inspect_middleware'
+require_relative 'restforce_middleware'
+require_relative 'stdout_middleware'
+require_relative 'twitter_middleware'
+require_relative 'undot_params_middleware'
