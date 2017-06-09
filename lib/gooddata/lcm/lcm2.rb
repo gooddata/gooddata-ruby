7ef4bcbadcf1bc7af645413ad38ce3ed132fc069
vhtien <tien.vo@gooddata.com>
2017-06-09 13:30:06 +0700

    REFACTOR TMA-478: Please optimize handling of CA attributes in LCM bricks

--- lib/gooddata/lcm/lcm2.rb
+++ lib/gooddata/lcm/lcm2.rb
@@ -85,12 +85,11 @@ module GoodData
         EnsureTechnicalUsersDomain,
         EnsureTechnicalUsersProject,
         SynchronizeLdm,
+        CollectMeta,
+        CollectTaggedObjects,
         CollectComputedAttributeMetrics,
         ImportObjectCollections,
         SynchronizeComputedAttributes,
-        CollectMeta,
-        CollectTaggedObjects,
-        ImportObjectCollections,
         SynchronizeLabelTypes,
         SynchronizeAttributeDrillpath,
         SynchronizeProcesses,
@@ -119,14 +118,11 @@ module GoodData
         EnsureTechnicalUsersDomain,
         EnsureTechnicalUsersProject,
         SynchronizeLdm,
-        CollectComputedAttributeMetrics,
-        ImportObjectCollections,
-        SynchronizeComputedAttributes, # need to sync CA here to maintain the drill path after that
         SynchronizeLabelTypes,
-        SynchronizeAttributeDrillpath,
         ApplyCustomMaql,
         SynchronizeColorPalette,
         SynchronizeClients,
+        SynchronizeComputedAttributes,
         SynchronizeETLsInSegment
       ]
     }
