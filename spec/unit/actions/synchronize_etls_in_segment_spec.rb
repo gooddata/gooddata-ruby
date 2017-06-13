ca677bccf7f6c38dcf90b83d79eb35bbe989f3a7
panjan <jan.zdrahal@gooddata.com>
2017-06-13 11:22:01 +0200

    Synchronize only additional hidden params
    RELATED: TMA-340

--- spec/unit/actions/synchronize_etls_in_segment_spec.rb
+++ spec/unit/actions/synchronize_etls_in_segment_spec.rb
@@ -9,8 +9,59 @@ require 'gooddata/lcm/actions/synchronize_etls_in_segment'
 
 describe GoodData::LCM2::SynchronizeETLsInSegment do
   let(:gdc_gd_client) { double('gdc_gd_client') }
+  let(:logger) { double('logger') }
   let(:domain) { double('domain') }
   let(:segment) { double('segment') }
+  let(:params) do
+    params = {
+      gdc_gd_client: gdc_gd_client,
+      gdc_logger: logger,
+      organization: domain,
+      synchronize: [{
+        segment_id: 'some_segment_ids',
+        to: [{ pid: 'some_target_project' }]
+      }]
+    }
+    GoodData::LCM2.convert_to_smart_hash(params)
+  end
+  let(:result) { { syncedResult: {} } }
+  let(:target_project) { double('target_project') }
+
+  before do
+    allow(gdc_gd_client).to receive(:domain) { domain }
+    allow(domain).to receive(:segments) { segment }
+    allow(segment).to receive(:synchronize_processes) { result }
+    allow(gdc_gd_client).to receive(:projects).and_return(target_project)
+  end
+
+  context 'when hidden parameters not in additional_hidden_params' do
+    let(:schedule) { double(:schedule) }
+    let(:hidden_params) { { hidden_param_1: 'foo', hidden_param_2: 'bar' } }
+
+    before do
+      allow(target_project).to receive(:schedules).and_return([schedule])
+      allow(schedule).to receive(:update_params)
+      allow(schedule).to receive(:update_hidden_params)
+      allow(schedule).to receive(:save)
+      allow(schedule).to receive(:hidden_params)
+        .and_return(hidden_params)
+      allow(logger).to receive(:warn)
+      allow(schedule).to receive(:hidden_params=)
+      allow(schedule).to receive(:enable)
+    end
+
+    it 'removes the parameters' do
+      expect(schedule).to receive(:hidden_params=).with({})
+      subject.class.call(params)
+    end
+
+    it 'warns the user' do
+      expect(logger).to receive(:warn) do |log|
+        expect(log).to include('hidden_param_1', 'hidden_param_2')
+      end
+      subject.class.call(params)
+    end
+  end
 
   context 'when sync processes/schedules has problem' do
     let(:result) do
@@ -26,22 +77,6 @@ describe GoodData::LCM2::SynchronizeETLsInSegment do
           ]
         }
       }
-    end
-
-    let(:params) do
-      params = {
-        gdc_gd_client: gdc_gd_client,
-        organization: domain,
-        synchronize: [{ segment_id: 'some_segment_ids' }]
-
-      }
-      GoodData::LCM2.convert_to_smart_hash(params)
-    end
-
-    before do
-      allow(gdc_gd_client).to receive(:domain) { domain }
-      allow(domain).to receive(:segments) { segment }
-      allow(segment).to receive(:synchronize_processes) { result }
     end
 
     it 'raise error' do
