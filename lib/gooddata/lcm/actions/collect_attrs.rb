2100baa596a2685f024a53eed76d279e4787b67c
vhtien <tien.vo@gooddata.com>
2017-06-30 15:17:15 +0700

    TMA-498 Release Brick: move SynchronizeLabelTypes and SynchronizeAttributeDrillpath to ImportObjectCollections

---
+++ lib/gooddata/lcm/actions/collect_attrs.rb
@@ -0,0 +1,56 @@
+# encoding: UTF-8
+#
+# Copyright (c) 2010-2017 GoodData Corporation. All rights reserved.
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+require_relative 'base_action'
+
+module GoodData
+  module LCM2
+    class CollectAttributes < BaseAction
+      DESCRIPTION = "Collect all attributes (include CAs) to transfer label type and drill path"
+
+      PARAMS = define_params(self) do
+        description 'Development Client Used for Connecting to GD'
+        param :development_client, instance_of(Type::GdClientType), required: true
+
+        description 'Synchronization Info'
+        param :synchronize, array_of(instance_of(Type::SynchronizationInfoType)), required: true, generated: true
+      end
+
+      class << self
+        def call(params)
+          results = []
+
+          development_client = params.development_client
+
+          synchronize = params.synchronize.pmap do |info|
+            from = info.from
+            from_project = development_client.projects(from) || fail("Invalid 'from' project specified - '#{from}'")
+            objects = (from_project.attributes.to_a + from_project.labels.to_a).map(&:uri)
+
+            info[:transfer_uris] ||= []
+            info[:transfer_uris] += objects
+
+            results += objects.map do |uri|
+              {
+                project: from,
+                transfer_uri: uri
+              }
+            end
+
+            info
+          end
+
+          {
+            results: results,
+            params: {
+              synchronize: synchronize
+            }
+          }
+        end
+      end
+    end
+  end
+end
