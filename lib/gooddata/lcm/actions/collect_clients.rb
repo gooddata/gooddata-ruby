3c1bdb993b5d1376cab8737dccb9964cc86b859b
Tomas Korcak <korczis@gmail.com>
2017-07-19 16:37:30 +0200

    Do not use quoted strings

--- lib/gooddata/lcm/actions/collect_clients.rb
+++ lib/gooddata/lcm/actions/collect_clients.rb
@@ -76,11 +76,9 @@ module GoodData
           clients = []
           data_source = GoodData::Helpers::DataSource.new(params.input_source)
           input_data = File.open(data_source.realize(params), 'r:bom|UTF-8')
-          data = input_data.read
-          puts data
-          GoodData.logger.debug("Input data: #{data}")
+          GoodData.logger.debug("Input data: #{input_data.read}")
           GoodData.logger.debug("Segment names: #{segment_names}")
-          CSV.foreach(input_data, :headers => true, :return_headers => false, encoding: 'bom|utf-8') do |row|
+          CSV.foreach(input_data, :headers => true, :return_headers => false, encoding: 'bom|utf-8', quote_char: nil) do |row|
             GoodData.logger.debug("Processing row: #{row}")
             segment_name = row[segment_id_column]
             GoodData.logger.debug("Segment name: #{segment_name}")
