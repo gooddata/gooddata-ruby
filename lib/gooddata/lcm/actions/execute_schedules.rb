e1d9c943aaeafd25abaa8786766ae79c2e3c4e40
vhtien <tien.vo@gooddata.com>
2017-08-29 15:31:12 +0700

    TMA-583: Regression issue in schedule execution brick

--- lib/gooddata/lcm/actions/execute_schedules.rb
+++ lib/gooddata/lcm/actions/execute_schedules.rb
@@ -32,6 +32,15 @@ module GoodData
 
         description 'Wait For Schedule'
         param :wait_for_schedule, instance_of(Type::StringType), required: false, default: 'false'
+
+        description 'Segment List'
+        param :segment_list, instance_of(Type::StringType), required: false
+
+        description 'Domain'
+        param :domain, instance_of(Type::StringType), required: false
+
+        description 'Number Of Threads'
+        param :number_of_threads, instance_of(Type::StringType), required: false, default: '5'
       end
 
       class << self
@@ -48,10 +57,26 @@ module GoodData
           control_parameter = params.control_parameter || "MODE"
           wait = GoodData::Helpers.to_boolean(params.wait_for_schedule)
 
+          segment_list = params.segment_list
+          domain_name  = params.domain
+          fail "In case that you are using SEGMENT_LIST parameter, you need to fill out DOMAIN parameter" if !segment_list.nil? && domain_name.nil?
+
+          number_of_threads = Integer(params.number_of_threads || '5')
+
           # The WORK_DONE_IDENTIFICATOR is flag which tells the executor to execute the schedules
           # It could have special value IGNORE. In this case all corresponding schedules will be started during every run of this brick
 
           start_schedules = work_done_identificator == 'IGNORE' || GoodData::Helpers.to_boolean(project.metadata[work_done_identificator])
+
+          project_list = []
+          if !segment_list.nil? && segment_list != ''
+            domain = client.domain(domain_name)
+            segment_list.split('|').each do |segment_identification|
+              segment = domain.segments(segment_identification)
+              project_list += segment.clients.map { |c| !c.project.nil? ? c.project.obj_id : nil }.compact
+            end
+            project_list.uniq!
+          end
 
           if start_schedules
             schedules_to_filter = client.projects.pmapcat do |p|
@@ -64,12 +89,17 @@ module GoodData
             end
 
             schedules_to_start = schedules_to_filter.select { |schedule| list_of_modes.include?(schedule.params[control_parameter]) }
+
+            if !segment_list.nil? && segment_list != ''
+              schedules_to_start.delete_if { |schedule| !project_list.include?(schedule.project.obj_id) }
+            end
+
             logger.info "Found #{schedules_to_start.count} schedules to execute"
 
             schedules_to_start.each_slice(number_of_schedules_in_batch).each_with_index do |batch_schedules, batch_index|
               batch_number = batch_index + 1
               logger.info "Starting batch number #{batch_number}. Number of schedules in batch #{batch_schedules.count}."
-              batch_schedules.each do |schedule|
+              batch_schedules.peach(number_of_threads) do |schedule|
                 begin
                   tries ||= 5
                   logger.info "Starting schedule for project #{schedule.project.pid} - #{schedule.project.title}. Schedule ID is #{schedule.obj_id}"
