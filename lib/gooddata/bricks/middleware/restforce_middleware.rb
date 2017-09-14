b5afa214bef1e1076ef9bec1f19c71470c956982
hexo <hacxman@gmail.com>
2017-09-14 11:12:29 +0200

    TMA-45: remove commented require from restforce_middleware

--- lib/gooddata/bricks/middleware/restforce_middleware.rb
+++ lib/gooddata/bricks/middleware/restforce_middleware.rb
@@ -4,9 +4,6 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
-# TODO: Remove this
-# require 'gooddata'
-
 require_relative 'base_middleware'
 
 module GoodData
