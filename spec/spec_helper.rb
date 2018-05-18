2c09257ea3a2f16f5aa8a107d4f4b77e68637542
Jiří Mikulášek <jiri.mikulasek@gooddata.com>
2018-05-18 10:04:15 +0200

    TMA-904: allow to enable/disable VCR completely by ENV

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -27,7 +27,9 @@ require 'rspec'
 require 'pathname'
 require 'webmock/rspec'
 require 'gooddata'
-require 'vcr_configurer'
+
+vcr_enabled = !ENV['VCR_ON'] || ENV['VCR_ON'] == 'true' # VCR is enabled by default - set VCR_ON=false to disable
+require 'vcr_configurer' if vcr_enabled
 
 logger = Logger.new(STDOUT)
 logger.level = Logger::WARN
@@ -65,29 +67,31 @@ RSpec.configure do |config|
 
   config.fail_fast = false
 
-  config.before(:all) do
-    # in case the test uses VCR
-    if self.class.metadata[:vcr]
-      # replace parallel iterations with the serial one, since VCR can't handle parallel request matching correctly
-      module Enumerable
-        def peach_with_index(*)
-          each_with_index
+  if vcr_enabled
+    config.before(:all) do
+      # in case the test uses VCR
+      if self.class.metadata[:vcr]
+        # replace parallel iterations with the serial one, since VCR can't handle parallel request matching correctly
+        module Enumerable
+          def peach_with_index(*)
+            each_with_index
+          end
         end
+
+        # insert the cassete recording everything what happens outside the tests cases
+        VCR.insert_cassette("#{self.class.metadata[:description]}/all")
       end
-
-      # insert the cassete recording everything what happens outside the tests cases
-      VCR.insert_cassette("#{self.class.metadata[:description]}/all")
     end
-  end
 
-  config.after(:all) do
-    # in case the test uses VCR
-    if self.class.metadata[:vcr]
-      # eject the cassete recording everything what happens outside the tests cases
-      VCR.eject_cassette
+    config.after(:all) do
+      # in case the test uses VCR
+      if self.class.metadata[:vcr]
+        # eject the cassete recording everything what happens outside the tests cases
+        VCR.eject_cassette
 
-      # reload the original parallel iterations
-      load('pmap.rb') if self.class.metadata[:vcr]
+        # reload the original parallel iterations
+        load('pmap.rb') if self.class.metadata[:vcr]
+      end
     end
   end
 end
