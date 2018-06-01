3d471e4303e6854c636a21f05bd7593d2544278d
panjan <jan.zdrahal@gooddata.com>
2018-06-01 15:32:20 +0200

    Skip sleep only when vcr_record_mode=none

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -66,7 +66,7 @@ RSpec.configure do |config|
 
   if ENV['VCR_ON'].nil? || ENV['VCR_ON'].downcase == 'true' # VCR is enabled by default - set VCR_ON=false to disable
     require 'vcr_configurer'
-    skip_sleep = ENV['VCR_RECORD_MODE'].nil? || ENV['VCR_RECORD_MODE'].downcase == 'none'
+    skip_sleep = ENV['VCR_RECORD_MODE'] && ENV['VCR_RECORD_MODE'].downcase == 'none'
 
     config.before(:all) do
       # in case the test uses VCR
