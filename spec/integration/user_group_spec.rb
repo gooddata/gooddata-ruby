19afc6f26ec52a0dbd46a83aee6ab34f969ec752
Jakub Mahnert <jakub.mahnert@gooddata.com>
2018-06-05 15:52:29 +0200

    fix: temporarily remove vcr for user groups

--- spec/integration/user_group_spec.rb
+++ spec/integration/user_group_spec.rb
@@ -4,7 +4,7 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
-describe GoodData::UserGroup, :vcr do
+describe GoodData::UserGroup do
   before(:all) do
     @bulk_size = 3
 
