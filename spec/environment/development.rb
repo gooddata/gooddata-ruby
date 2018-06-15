7e316c79e2e6e9771ef6c2253f6dbfd6eb596ea6
panjan <jan.zdrahal@gooddata.com>
2018-06-15 13:36:44 +0200

    Deduplicate environments
    Duplicit environments were introduced by the merge of AppStore to gooddata-ruby.
    This change reduces the duplication by saving the environments in one
    place (i.e. the directory spec/environment/performance).

--- spec/environment/development.rb
+++ spec/environment/development.rb
@@ -15,6 +15,17 @@ module GoodData
       set_const :DEFAULT_SERVER, 'https://staging3-lcm-prod.intgdc.com'
       set_const :DEFAULT_USER_URL, '/gdc/account/profile/a4c644d7b42b65c34e5a0f46809f7164'
       set_const :STAGING_URI, 'https://staging3-lcm-prod.intgdc.com/gdc/uploads/'
+      set_const :LCM_ENVIRONMENT,
+                dev_server: 'staging3-lcm-dev.intgdc.com',
+                prod_server: 'staging3-lcm-prod.intgdc.com',
+                dev_token: "krw3m2jQREVy9GRJdJi4f4sXcF6r/L515s3Frv8l4eY=\n",
+                prod_token: "qq9Mgu0OPqZDpKJyG7R2SM20uvL5Kho+8eAGTvkSTuM=\n",
+                vertica_dev_token: "wZ3cvWN8XT40aV9x8xzigrXSbrYGwhH8FaEf6m6IqkA=\n",
+                vertica_prod_token: "VV4J66eCRu74qip2ZH2/OVaqgO8gCZ655xXQgiTfrKo=\n",
+                dev_organization: 'staging3-lcm-dev',
+                prod_organization: 'staging3-lcm-prod',
+                username: 'rubydev+admin@gooddata.com',
+                password:  "8dP7cCR0LqAyyo4S817bt8bHKfuIteVCW4Y76sGkx78=\n"
     end
 
     module ProcessHelper
