cbfcb56f22fa2ec9ce8af54fa3fadbda2b492d2d
panjan <jan.zdrahal@gooddata.com>
2018-07-04 16:17:59 +0200

    TMA-979: Reduce log level to info in bricks
    before it was debug and bricks would produce
    enormous logs

--- spec/unit/bricks/middleware/logger_middleware_spec.rb
+++ spec/unit/bricks/middleware/logger_middleware_spec.rb
@@ -9,22 +9,47 @@ require 'gooddata/bricks/bricks'
 require 'gooddata/bricks/middleware/logger_middleware'
 
 describe GoodData::Bricks::LoggerMiddleware do
+  let(:app) { double(:app) }
+  before do
+    subject.app = app
+    allow(app).to receive(:call)
+  end
+
   it "Has GoodData::Bricks::LoggerMiddleware class" do
     GoodData::Bricks::LoggerMiddleware.should_not be(nil)
   end
 
   context 'when HTTP_LOGGING parameter set to true' do
     let(:params) { { 'HTTP_LOGGING' => 'true' } }
-    let(:app) { double(:app) }
-
-    before do
-      subject.app = app
-      allow(app).to receive(:call)
-    end
 
     it 'turns http logging on' do
       expect(GoodData).to receive(:logging_http_on)
       subject.call(params)
+    end
+  end
+
+  context 'GDC_LOG_LEVEL' do
+    let(:logger) { double(Logger) }
+    let(:params) { { 'GDC_LOG_LEVEL' => log_level } }
+    before do
+      allow(Logger).to receive(:new) { logger }
+      allow(logger).to receive(:info)
+    end
+
+    context 'when set' do
+      let(:log_level) { 'warn' }
+      it 'sets the specified log level' do
+        expect(logger).to receive(:level=).with(log_level)
+        subject.call(params)
+      end
+    end
+
+    context 'when not set' do
+      let(:log_level) { nil }
+      it 'sets info log level' do
+        expect(logger).to receive(:level=).with('info')
+        subject.call(params)
+      end
     end
   end
 end
