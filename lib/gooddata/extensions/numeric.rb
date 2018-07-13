f784043d46ce08f4322b3b09e286a8b5d3eba2fa
Henrich Moraes <henrich.moraes@resultadosdigitais.com.br>
2018-07-13 15:16:16 -0300

    Isolate Numeric monkeypatch in NumericExtensions module

--- lib/gooddata/extensions/numeric.rb
+++ lib/gooddata/extensions/numeric.rb
@@ -1,15 +1,14 @@
-# encoding: UTF-8
-#
 # Copyright (c) 2010-2017 GoodData Corporation. All rights reserved.
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
-
-class Numeric
-  # Numbers are not duplicable:
-  #
-  #  3.duplicable? # => false
-  #  3.dup         # => TypeError: can't dup Fixnum
-  def duplicable?
-    false
+module NumericExtensions
+  refine Numeric do
+    # Numbers are not duplicable:
+    #
+    #  3.duplicable? # => false
+    #  3.dup         # => TypeError: can't dup Fixnum
+    def duplicable?
+      false
+    end
   end
 end
--- lib/gooddata/helpers/global_helpers.rb
+++ lib/gooddata/helpers/global_helpers.rb
@@ -16,6 +16,7 @@ require 'gooddata/extensions/true'
 require 'gooddata/extensions/false'
 require 'gooddata/extensions/integer'
 require 'gooddata/extensions/nil'
+require 'gooddata/extensions/numeric'
 require 'gooddata/extensions/big_decimal'
 
 module GoodData
@@ -34,6 +35,7 @@ module GoodData
       using FalseExtensions
       using IntegerExtensions
       using NilExtensions
+      using NumericExtensions
       using BigDecimalExtensions
 
       def error(msg)
