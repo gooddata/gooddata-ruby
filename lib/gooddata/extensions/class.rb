df5136fe7e2a2ca32f4083353f6b056355f56d63
Henrich Moraes <henrich.moraes@resultadosdigitais.com.br>
2018-07-13 15:37:31 -0300

    Code :lipstick: Insert license header in files where it was missing

--- lib/gooddata/extensions/class.rb
+++ lib/gooddata/extensions/class.rb
@@ -1,3 +1,6 @@
+# Copyright (c) 2010-2017 GoodData Corporation. All rights reserved.
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
 module ClassExtensions
   refine Class do
     def short_name
--- lib/gooddata/extensions/integer.rb
+++ lib/gooddata/extensions/integer.rb
@@ -1,3 +1,6 @@
+# Copyright (c) 2010-2017 GoodData Corporation. All rights reserved.
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
 module IntegerExtensions
   refine Integer do
     def to_b
