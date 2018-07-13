1bbedbd655fb64ef72a398f0a7e4754e0558192b
Henrich Moraes <henrich.moraes@resultadosdigitais.com.br>
2018-07-13 01:49:56 -0300

    Remove monkey patchs from Hash

--- lib/gooddata/extensions/hash.rb
+++ lib/gooddata/extensions/hash.rb
@@ -5,38 +5,7 @@
 # LICENSE file in the root directory of this source tree.
 
 class Hash
-  # Return a hash that includes everything but the given keys. This is useful for
-  # limiting a set of parameters to everything but a few known toggles:
-  #
-  #   @person.update_attributes(params[:person].except(:admin))
-  #
-  # If the receiver responds to +convert_key+, the method is called on each of the
-  # arguments. This allows +except+ to play nice with hashes with indifferent access
-  # for instance:
-  #
-  #   {:a => 1}.with_indifferent_access.except(:a)  # => {}
-  #   {:a => 1}.with_indifferent_access.except("a") # => {}
-  #
-  def except(*keys)
-    dup.except!(*keys)
-  end
-
-  # Replaces the hash without the given keys.
-  def except!(*keys)
-    keys.each { |key| delete(key) }
-    self
-  end
-
-  def slice(*keys)
-    keys.map! { |key| convert_key(key) } if respond_to?(:convert_key, true)
-    keys.each_with_object(self.class.new) { |k, hash| hash[k] = self[k] if key?(k) }
-  end
-
-  def compact
-    select { |_, value| !value.nil? }
-  end
-
-  def deep_merge(hash)
+  def custom_deep_merge(hash)
     hash = hash.to_hash
     merge(hash) do |_key, old_val, new_val|
       if old_val.is_a?(Hash) && new_val.is_a?(Hash)
