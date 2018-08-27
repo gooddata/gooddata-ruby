5cd2d695c7d1cb9b4dba0bf702a48c423c866066
panjan <jan.zdrahal@gooddata.com>
2018-08-27 10:19:13 +0200

    Add empty lines between licenses and modules
    and classes so that documentation generators
    don't pick up license headers as module/class
    documentation.

--- spec/environment/environment.rb
+++ spec/environment/environment.rb
@@ -3,6 +3,7 @@
 # Copyright (c) 2010-2017 GoodData Corporation. All rights reserved.
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
+
 module GoodData
   module Environment
     class << self
