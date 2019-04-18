e62b6fea4b664e1602d9e1d52aa36c235d727e91
panjan <jan.zdrahal@gooddata.com>
2019-04-18 10:00:29 +0200

    fixup! fixup! fixup! TMA-906: rollout/provisioning bricks set dynamic params from ads

--- spec/lcm/integration/shared_examples_for_provisioning_and_rollout.rb
+++ spec/lcm/integration/shared_examples_for_provisioning_and_rollout.rb
@@ -2,13 +2,17 @@ shared_examples 'a provisioning/rollout brick' do
   it 'sets dynamic parameters' do
     projects.each do |target_project|
       target_schedules = target_project.schedules
+      client_id = @workspaces.find { |w| w[:title] == target_project.title }[:client_id]
       target_schedules.each do |target_schedule|
         require 'pry';binding.pry
-        all_dynamic_param = target_schedule.params[Support::ALL_DYNAMIC_PARAMS_KEY] == Support::ALL_DYNAMIC_PARAMS_VALUE
-        expect(all_dynamic_param).to be(true)
-        client_id = @workspaces.find { |w| w[:title] == target_project.title }[:client_id]
+        all_dynamic_param = target_schedule.params[Support::ALL_DYNAMIC_PARAMS_KEY]
+        expect(all_dynamic_param).to eq(Support::ALL_DYNAMIC_PARAMS_VALUE)
         dynamic_param = target_schedule.params[Support::DYNAMIC_PARAMS_KEY]
-        expect(dynamic_param).to eq(client_id)
+        if target_schedule.name == RUBY_HELLO_WORLD_SCHEDULE_NAME
+          expect(dynamic_param).to eq(client_id)
+        else
+          expect(dynamic_param).to be_nil
+        end
       end
     end
   end
