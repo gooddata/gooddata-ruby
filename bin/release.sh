e103b65407fae6cbce7a2d0a0149ef9cab504c72
Hung Nguyen <hung.nguyen@gooddata.com>
2025-06-23 10:16:51 +0700

    GRIF-177: Remove setting TLS1.2 for LCM release brick

--- bin/release.sh
+++ bin/release.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-export JRUBY_OPTS="-J-XX:MaxRAMPercentage=75 -J-Djdk.tls.client.protocols=TLSv1.2 -J-Dhttps.protocols=TLSv1.2"
+export JRUBY_OPTS=-J-XX:MaxRAMPercentage=75
 
 /bin/bash -l -c "bundle exec ./bin/run_brick.rb release_brick"
